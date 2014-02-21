31320335dc36afefa2853299fe2cef5fbe070450
hpyhacking <crazyflapjack@gmail.com>
2014-02-22 02:59:16 +0800

    fixed deposits and settings view

--- app/models/deposit_channel.rb
+++ app/models/deposit_channel.rb
@@ -7,4 +7,20 @@ class DepositChannel < ActiveYaml::Base
   def self.currency(category)
     self.find_by_id(category).currency.to_s
   end
+
+  def transfer_text
+    I18n.t("peatio.deposit_channel.#{id}.transfer")
+  end
+
+  def latency_text
+    I18n.t("peatio.deposit_channel.#{id}.latency") 
+  end
+
+  def name_text
+    I18n.t("peatio.deposit_channel.#{id}.name")
+  end
+
+  def intro_text
+    I18n.t("peatio.deposit_channel.#{id}.intro")
+  end
 end
