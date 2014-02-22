b76d263ee46e930fa59b59ca15ded7ddf8fd2059
Wei Lu <luwei.here@gmail.com>
2014-02-22 13:44:14 +0800

    move status groups into accordion

--- app/controllers/admin/statistic/orders_controller.rb
+++ app/controllers/admin/statistic/orders_controller.rb
@@ -5,7 +5,7 @@ module Admin
         @orders_grid = ::Statistic::OrdersGrid.new(params[:statistic_orders_grid])
         @assets = @orders_grid.assets
 
-        @groups = { 
+        @groups = {
           :count => @assets.size,
           :sum => @assets.sum(:origin_volume),
           :avg => (@assets.average(:price) || 0.to_d).truncate(2),
