a8612da110082a6977eb9a7070c4f6a077d8f788
Jan Xie <jan.h.xie@gmail.com>
2014-03-10 16:00:47 +0800

    mock resque enqueue in ordering spec

--- spec/services/ordering_spec.rb
+++ spec/services/ordering_spec.rb
@@ -5,12 +5,16 @@ describe Ordering do
   let(:account) { create(:account, balance: 100.to_d, locked: 100.to_d) }
 
   describe "ordering service can submit order" do
-    before do order.stubs(:hold_account).returns(account) end
-    it { expect(Ordering.new(order).submit).to_not be_false }
+    before do
+      order.stubs(:hold_account).returns(account)
+      Resque.expects(:enqueue).with(Job::Matching, order.to_matching_attributes)
+    end
+
+    it {expect(Ordering.new(order).submit).to be_true }
   end
 
   describe "ordering service can cancel order" do
     before do order.stubs(:hold_account).returns(account) end
-    it { expect(Ordering.new(order).cancel).to_not be_false }
+    it { expect(Ordering.new(order).cancel).to be_true }
   end
 end
