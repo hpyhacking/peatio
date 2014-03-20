7bd7f492d0bf9c655a542fac8d16b48c837e21a0
Jan Xie <jan.h.xie@gmail.com>
2014-03-20 18:49:30 +0800

    show btc assets

--- app/controllers/private/assets_controller.rb
+++ app/controllers/private/assets_controller.rb
@@ -3,6 +3,7 @@ module Private
     before_action :auth_activated!
 
     def index
+      @btc_addresses = Currency.addresses['btc']
       @btc_proof = Proof.with_currency(:btc).last
       @cny_proof = Proof.with_currency(:cny).last
       @btc_account = current_user.accounts.with_currency(:btc).first
