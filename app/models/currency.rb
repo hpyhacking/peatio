8c600f56810402df9d4df7b24471322ff4ca6daa
Jan Xie <jan.h.xie@gmail.com>
2014-03-25 12:34:58 +0800

    remove useless Currency.trade_config

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -13,19 +13,6 @@ class Currency < Settingslogic
     @codes ||= self.currencies.symbolize_keys
   end
 
-  def self.trade_config(code)
-    @trade_configs ||= self.trades.map do |d|
-      {d["name"] => d} 
-    end.inject do 
-      |sum, x| sum.merge x 
-    end.symbolize_keys
-
-    @trade_configs[code.to_sym].symbolize_keys.tap do |conf|
-      conf[:bid][:fee] = conf[:bid][:fee].to_d / 10000.to_d
-      conf[:ask][:fee] = conf[:ask][:fee].to_d / 10000.to_d
-    end
-  end
-
   source "#{Rails.root}/config/currency.yml"
   namespace Rails.env
   suppress_errors Rails.env.production?
