c376d1a861a05928a891b32bf435c3dec2ec8541
tomlion <qycpublic@gmail.com>
2014-03-31 00:46:49 +0800

    Ops, shouldn't set timezone on this branch

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module Peatio
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Beijing'
 
     config.i18n.enforce_available_locales = false
 
