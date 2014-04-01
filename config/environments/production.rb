d5ac1d9e86cd985b5e517a00ef92b484301ef6d1
root <root@li409-128.members.linode.com>
2014-04-02 02:54:07 +0800

    Change log level to warn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Peatio::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :warn
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
