57bcca7b41359a59f55fdcc64ffcc791e66aaa00
Jan Xie <jan.h.xie@gmail.com>
2014-04-01 23:47:13 +0800

    fix proof verify button

--- app/helpers/private/assets_helper.rb
+++ app/helpers/private/assets_helper.rb
@@ -5,4 +5,10 @@ module Private::AssetsHelper
     link_to addr, btc_block_url(addr)
   end
 
+  def verify_link(proof, account)
+    hashtag = "verify?partial_tree=#{account.partial_tree.to_json}&expected_root=#{proof.root.to_json}"
+    uri = "http://syskall.com/proof-of-liabilities/##{URI.encode hashtag}"
+    link_to t('.go-verify'), uri, :class => 'btn btn-default', :target => '_blank'
+  end
+
 end
