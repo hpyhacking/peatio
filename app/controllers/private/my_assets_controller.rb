64f5a83c6cb3a354add83640a1eb89a625cf0514
Wei Lu <luwei.here@gmail.com>
2014-04-03 18:28:19 +0800

    deposit & withdraw grid first pass

--- app/controllers/private/my_assets_controller.rb
+++ app/controllers/private/my_assets_controller.rb
@@ -2,6 +2,27 @@ module Private
   class MyAssetsController < BaseController
     def index
       @accounts = current_user.accounts
+      load_deposits
+      load_withdraws
+    end
+
+    private
+    def load_deposits
+      page = params[:page] || 0
+      per = params[:per] || 10
+
+      @deposits_grid = PrivateDepositsGrid.new(params[:deposits_grid]) do |scope|
+        scope.where(member: current_user).page(page).per(per)
+      end
+    end
+
+    def load_withdraws
+      page = params[:page] || 0
+      per = params[:per] || 10
+
+      @withdraws_grid = WithdrawsGrid.new(params[:withdraws_grid]) do |scope|
+        scope.where(member: current_user).page(page).per(per)
+      end
     end
   end
 end
