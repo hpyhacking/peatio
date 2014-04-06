e5fe25c964d3ac1dcb48f936271c559750877d32
tomlion <qycpublic@gmail.com>
2014-04-07 07:20:41 +0800

    TwoFactor for withdraw

--- app/controllers/private/base_controller.rb
+++ app/controllers/private/base_controller.rb
@@ -1,5 +1,17 @@
 module Private
   class BaseController < ::ApplicationController
     before_filter :auth_member!
+
+    private
+
+    def verify_two_factor!
+      return true unless current_user.two_factor_activated?
+
+      two_factor = current_user.two_factor
+      unless two_factor.verify(params[:two_factor][:otp])
+        flash[:notice] = two_factor.errors[:otp].join
+        redirect_to :back
+      end
+    end
   end
 end
