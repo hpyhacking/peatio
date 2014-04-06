30ab374fa1a2752f715c6e6216e76c451af10d8d
Jan Xie <jan.h.xie@gmail.com>
2014-04-06 14:51:16 +0800

    expose order executed and remaining volume in api

--- app/api/api_v2/entities/order.rb
+++ app/api/api_v2/entities/order.rb
@@ -5,13 +5,18 @@ module APIv2
 
       expose :id
       expose :price
-      expose :origin_volume, as: :volume
       expose :state
       expose :currency, as: :market
       expose :created_at, format_with: :iso8601
 
+      expose :origin_volume, as: :volume
+      expose :volume, as: :remaining_volume
+      expose :executed_volume do |order, options|
+        order.origin_volume - order.volume
+      end
+
       expose :side do |order, options|
-        order.type[-3, 3] == 'Ask' ? 'Sell' : 'Buy'
+        order.type[-3, 3] == 'Ask' ? 'sell' : 'buy'
       end
     end
   end
--- spec/api/api_v2/entities/order_spec.rb
+++ spec/api/api_v2/entities/order_spec.rb
@@ -2,16 +2,18 @@ require 'spec_helper'
 
 describe APIv2::Entities::Order do
 
-  let(:order) { create(:order_ask, currency: 'btccny', price: '12.326'.to_d, volume: '123.123456789') }
+  let(:order) { create(:order_ask, currency: 'btccny', price: '12.326'.to_d, volume: '3.14', origin_volume: '12.13') }
 
   subject { OpenStruct.new APIv2::Entities::Order.represent(order, {}).serializable_hash }
 
-  its(:id)         { should == order.id }
-  its(:price)      { should == order.price }
-  its(:volume)     { should == order.origin_volume }
-  its(:state)      { should == order.state }
-  its(:market)     { should == order.market }
-  its(:created_at) { should == order.created_at.iso8601 }
-  its(:side)       { should == 'Sell' }
+  its(:id)               { should == order.id }
+  its(:price)            { should == order.price }
+  its(:volume)           { should == order.origin_volume }
+  its(:remaining_volume) { should == order.volume }
+  its(:executed_volume)  { should == (order.origin_volume - order.volume)}
+  its(:state)            { should == order.state }
+  its(:market)           { should == order.market }
+  its(:created_at)       { should == order.created_at.iso8601 }
+  its(:side)             { should == 'sell' }
 
 end
