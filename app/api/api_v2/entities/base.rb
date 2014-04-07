83d8b913499931a3ba2a0aafab2dd13d1e50840f
Jan Xie <jan.h.xie@gmail.com>
2014-04-07 10:59:32 +0800

    add Account entity

--- app/api/api_v2/entities/base.rb
+++ app/api/api_v2/entities/base.rb
@@ -2,6 +2,7 @@ module APIv2
   module Entities
     class Base < Grape::Entity
       format_with(:iso8601) {|t| t.iso8601 }
+      format_with(:decimal) {|d| d.to_s('F') }
     end
   end
 end
--- spec/api/api_v2/entities/member_spec.rb
+++ spec/api/api_v2/entities/member_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe APIv2::Entities::Member do
 
-  let(:member) { create(:member) }
+  let(:member) { create(:verified_member) }
 
   subject { OpenStruct.new APIv2::Entities::Member.represent(member).serializable_hash }
 
   its(:sn)        { should == member.sn }
   its(:name)      { should == member.name }
   its(:email)     { should == member.email }
-  its(:activated) { should == member.activated }
+  its(:activated) { should == true }
 
 end
