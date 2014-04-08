6b0b24bff81ea70798dfb288fe08fe1d58af9f79
Daniel Lv <lgn21st@gmail.com>
2014-04-09 06:38:21 +0800

    Do not redirect to market

--- app/controllers/private/settings_controller.rb
+++ app/controllers/private/settings_controller.rb
@@ -7,10 +7,6 @@ module Private
 
       @two_factor = (current_user.two_factor and current_user.two_factor_activated?)
       @verified = (current_user.id_document and current_user.id_document_verified?)
-
-      if current_user.activated? and @verified
-        redirect_to market_path(latest_market) and return
-      end
     end
   end
 end
