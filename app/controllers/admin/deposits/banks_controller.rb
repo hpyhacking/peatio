52bf5ed23c633d708b3a1ea294f62e5e2c2dc872
hpyhacking <crazyflapjack@gmail.com>
2014-04-08 08:17:20 +0800

    bank deposit feature

--- app/controllers/admin/deposits/banks_controller.rb
+++ app/controllers/admin/deposits/banks_controller.rb
@@ -13,6 +13,38 @@ module Admin
           with_aasm_state(:submitting, :warning, :submitted).
           order('id DESC')
       end
+
+      def show
+        if params[:target]
+          @target = ::Deposits::Bank.new(target_params)
+          @match = @bank.sn == @target.sn &&
+            @bank.member.name == @target.holder &&
+            @bank.amount == @target.amount
+          flash.now[:notice] = t('.notice') if @bank.aasm_state.accepted?
+        else
+          @match = false
+          flash.now[:alert] = t('.alert') if @bank.aasm_state.accepted?
+        end
+      end
+
+      def update
+        raise 'unknown txid' unless params[:txid]
+
+        ActiveRecord::Base.transaction do
+          @bank.lock!
+          @bank.submit!
+          @bank.accept!
+          @bank.touch(:done_at)
+          @bank.update_attribute(:txid, params[:txid])
+        end
+
+        redirect_to :back
+      end
+
+      private
+      def target_params
+        params.require(:target).permit(:sn, :holder, :amount, :created_at, :txid)
+      end
     end
   end
 end
