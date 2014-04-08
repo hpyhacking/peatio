463bc0cb4b9b584a5c8037e3b4a42e89e014a4a4
Daniel Lv <lgn21st@gmail.com>
2014-04-09 01:35:20 +0800

    Fixed js error leads to non results at my-assets

--- app/controllers/private/my_assets_controller.rb
+++ app/controllers/private/my_assets_controller.rb
@@ -9,6 +9,7 @@ module Private
     private
 
     def load_transactions
+      @market = Market.find(latest_market)
       @deposits = Deposit.where(member: current_user).with_aasm_state(:accepted)
       @withdraws = Withdraw.where(member: current_user).with_aasm_state(:done)
       @buys = Trade.where(bid_member_id: current_user.id)
