8fbf86c617188608f69ae73031a2336d27ed8790
Jan Xie <jan.h.xie@gmail.com>
2014-04-10 15:10:19 +0800

    make sure generated key is unique

--- app/models/api_token.rb
+++ app/models/api_token.rb
@@ -9,8 +9,13 @@ class APIToken < ActiveRecord::Base
   private
 
   def generate_keys
-    self.access_key = APIv2::Auth::Utils.generate_access_key
-    self.secret_key = APIv2::Auth::Utils.generate_secret_key
+    begin
+      self.access_key = APIv2::Auth::Utils.generate_access_key
+    end while APIToken.where(access_key: access_key).any?
+
+    begin
+      self.secret_key = APIv2::Auth::Utils.generate_secret_key
+    end while APIToken.where(secret_key: secret_key).any?
   end
 
 end
