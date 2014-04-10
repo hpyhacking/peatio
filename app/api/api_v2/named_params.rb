6ee32e2ff0dd52d4f7cc9b586e441b16ff10205e
Jan Xie <jan.h.xie@gmail.com>
2014-04-10 15:20:28 +0800

    fix tonce document

--- app/api/api_v2/named_params.rb
+++ app/api/api_v2/named_params.rb
@@ -4,7 +4,7 @@ module APIv2
 
     params :auth do
       requires :access_key, type: String,  desc: "Access key."
-      requires :tonce,      type: Integer, desc: "Tonce is an integer represents the seconds elapsed since Unix epoch."
+      requires :tonce,      type: Integer, desc: "Tonce is an integer represents the milliseconds elapsed since Unix epoch."
       requires :signature,  type: String,  desc: "The signature of your request payload, generated using your secret key."
     end
 
