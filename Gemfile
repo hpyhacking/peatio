ec6cdbc0805999592e6802b76877e7fcefec5e3e
Daniel Lv <lgn21st@gmail.com>
2014-04-18 02:33:54 +0800

    Got rid of Twilio

--- Gemfile
+++ Gemfile
@@ -64,7 +64,6 @@ gem 'liability-proof', '0.0.8'
 gem 'whenever', '~> 0.9.2'
 gem 'easy_table'
 gem 'phonelib'
-gem 'twilio-ruby', '~> 3.11'
 
 group :development, :test do
   gem 'factory_girl_rails'
--- app/models/job/sms.rb
+++ app/models/job/sms.rb
@@ -5,15 +5,6 @@ module Job
 
     class << self
       def perform(phone_number, sms_message)
-        twilio_client.account.sms.messages.create(
-          from: ENV["TWILIO_NUMBER"],
-          to:   phone_number,
-          body: sms_message
-        )
-      end
-
-      def twilio_client
-        Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
       end
     end
 
