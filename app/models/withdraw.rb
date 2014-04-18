96417aec8ce00841908dec5c0734a14aa56d085f
hpyhacking <crazyflapjack@gmail.com>
2014-04-18 22:36:56 +0800

    fixed coin?

--- app/models/withdraw.rb
+++ app/models/withdraw.rb
@@ -23,6 +23,7 @@ class Withdraw < ActiveRecord::Base
   delegate :key_text, to: :channel, prefix: true
   delegate :id, to: :channel, prefix: true
   delegate :name, to: :member, prefix: true
+  delegate :coin?, to: :currency_obj
 
   before_validation :calc_fee
   before_validation :set_account
@@ -53,18 +54,6 @@ class Withdraw < ActiveRecord::Base
 
   def channel_name
     channel.key
-  end
-
-  def currency_symbol
-    case channel.currency
-    when 'btc' then 'B⃦'
-    when 'cny' then '¥'
-    else ''
-    end
-  end
-
-  def coin?
-    ['btc'].include? currency
   end
 
   def fiat?
