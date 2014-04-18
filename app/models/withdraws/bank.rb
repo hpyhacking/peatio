8bba4e57502ac2e2a072751845f7608e9f7fe36a
hpyhacking <crazyflapjack@gmail.com>
2014-04-18 23:38:31 +0800

    bank withdraw min 100

--- app/models/withdraws/bank.rb
+++ app/models/withdraws/bank.rb
@@ -2,6 +2,6 @@ module Withdraws
   class Bank < ::Withdraw
     include ::AasmAbsolutely
     include ::Withdraws::Bankable
-    validates :sum, presence: true, numericality: {greater_than: 1}, on: :create
+    validates :sum, presence: true, numericality: {greater_than_or_equal_to: 100}, on: :create
   end
 end
