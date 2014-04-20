b61500197d10a5e0c35d9a442bef58b02f84351f
James Chen <ashchan@gmail.com>
2014-04-20 11:07:14 +0900

    Support query of hierarchy of I18n keys

--- lib/peatio/i18n/backend/sqlite.rb
+++ lib/peatio/i18n/backend/sqlite.rb
@@ -18,15 +18,47 @@ module Peatio
           super
         end
 
+        def default_separator
+          "."
+        end
+
         protected
 
           def lookup(locale, key, scope = [], options = {})
             if record = Models::Translation.find_by(key: key)
-              value = record.send(locale) rescue ""
-              value.presence || record.en
-            else
-              nil
+              value_from_record(record, locale)
+            elsif children = Models::Translation.where(["key like ?", "#{key}#{default_separator}%"])
+              hashify_records(key, children.to_a, locale)
             end
+          end
+
+          def hashify_records(root_key, children, locale)
+            return nil if children.empty?
+
+            hash = children.inject({}) do |result, child|
+              key = child.key.gsub(Regexp.new("^" + Regexp.escape("#{root_key}#{default_separator}")), "")
+
+              if key.index(default_separator)
+                next_root = key.slice(0, key.index(default_separator))
+                next_root_key = root_key + default_separator + next_root
+                result[next_root.to_sym] = hashify_records(
+                  next_root_key,
+                  children.select { |r| r.key.starts_with? next_root_key },
+                  locale
+                )
+              else
+                result[key.to_sym] = value_from_record(child, locale)
+              end
+
+              result
+            end
+
+            hash
+          end
+
+          def value_from_record(record, locale)
+            value = record.send(locale) rescue ""
+            value.presence || record.en # fall back to :en
           end
       end
     end
