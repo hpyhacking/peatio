4d8edf0b95376cf764e1517c30a93e3f93aaabeb
Daniel Lv <lgn21st@gmail.com>
2014-04-22 17:51:27 +0800

    Add sms queue to startup script

--- start_resque.sh
+++ start_resque.sh
@@ -1,2 +1,2 @@
 RAILS_ENV=production PIDFILE=./log/resque.pid BACKGROUND=yes bundle exec rake environment resque:matching
-RAILS_ENV=production PIDFILE=./log/other_resque.pid BACKGROUND=yes QUEUE=coin,examine,mailer bundle exec rake environment resque:work
+RAILS_ENV=production PIDFILE=./log/other_resque.pid BACKGROUND=yes QUEUE=coin,examine,mailer,sms bundle exec rake environment resque:work
