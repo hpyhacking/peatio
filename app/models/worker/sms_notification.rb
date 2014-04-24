7ff339e397f37ce5e545bbb5e0a504619df133cb
Daniel Lv <lgn21st@gmail.com>
2014-04-24 20:26:12 +0800

    Using ChinaSMS to delivery SMS instead of Twilio

--- app/models/worker/sms_notification.rb
+++ app/models/worker/sms_notification.rb
@@ -4,15 +4,8 @@ module Worker
     def process(payload, metadata, delivery_info)
       payload.symbolize_keys!
 
-      twilio_client.account.sms.messages.create(
-        from: ENV["TWILIO_NUMBER"],
-        to:   payload[:phone],
-        body: payload[:message]
-      )
-    end
-
-    def twilio_client
-      Twilio::REST::Client.new ENV["TWILIO_SID"], ENV["TWILIO_TOKEN"]
+      ChinaSMS.use :juxin, username: ENV['JUXIN_USERNAME'], password: ENV['JUXIN_PASSWORD']
+      ChinaSMS.to payload[:phone], payload[:message]
     end
 
   end
