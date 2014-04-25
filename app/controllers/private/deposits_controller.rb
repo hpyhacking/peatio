b40bfa572d2cab61fc270d0d6518366ca98746d5
Daniel Lv <lgn21st@gmail.com>
2014-04-25 17:12:08 +0800

    Verified user before deposit.

--- app/controllers/private/deposits_controller.rb
+++ app/controllers/private/deposits_controller.rb
@@ -1,5 +1,8 @@
 module Private
   class DepositsController < BaseController
+    before_action :auth_activated!
+    before_action :auth_verified!
+
     def index
       @deposits = DepositChannel.all.sort
     end
