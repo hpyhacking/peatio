0c9a4655ecb7b1bda23ab934efe8efeffe47fa43
Daniel Lv <lgn21st@gmail.com>
2014-04-25 15:41:17 +0800

    ActionMailer using ENV["URL_HOST"] instead of peatio.dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,7 +23,7 @@ Peatio::Application.configure do
   config.action_mailer.delivery_method = :file
   config.action_mailer.file_settings = { location: 'tmp/mails' }
 
-  config.action_mailer.default_url_options = { :host => "peatio.dev" }
+  config.action_mailer.default_url_options = { :host => ENV["URL_HOST"] }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
