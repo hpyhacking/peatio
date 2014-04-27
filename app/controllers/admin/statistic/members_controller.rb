f5a06596dac887dbaf7a90e45b375143d920c71b
zhukejun <zhukejun78@hotmail.com>
2014-04-27 21:01:40 +0800

    Update members_controller.rb
    spell wrong

--- app/controllers/admin/statistic/members_controller.rb
+++ app/controllers/admin/statistic/members_controller.rb
@@ -2,7 +2,7 @@ module Admin
   module Statistic
     class MembersController < BaseController
       def show
-        @q = Member.order('id desc').includes(:two_factor, :id_document).search(params[:q])
+        @q = Member.order('id desc').includes(:two_factors, :id_document).search(params[:q])
         result = @q.result(distinct: true)
         @result_count = result.size
         @members_count = Member.all.size
