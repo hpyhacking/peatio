8e820c1b9d200b65615dfc4119de1ce6ebbf4d88
Daniel Lv <lgn21st@gmail.com>
2014-05-09 03:03:07 +0800

    add protocol to action_mailer's default_url_options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Peatio::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => ENV["URL_HOST"] }
+  config.action_mailer.default_url_options = { host: ENV["URL_HOST"], protocol: ENV['URL_SCHEMA'] }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
