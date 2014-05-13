15808612647836bd3b661d5b2535a1a1378502a6
Daniel Lv <lgn21st@gmail.com>
2014-05-13 23:08:08 +0800

    Update transaction fee for dogecoin

--- app/models/withdraws/dogecoin.rb
+++ app/models/withdraws/dogecoin.rb
@@ -4,5 +4,9 @@ module Withdraws
     include ::Withdraws::Coinable
 
     validates :sum, presence: true, numericality: {greater_than: 1}, on: :create
+
+    def set_fee
+      self.fee = 1.to_d
+    end
   end
 end
