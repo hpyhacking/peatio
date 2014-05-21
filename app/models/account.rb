baf7230fced7de5dd40b2c39fc05df3eda0e9081
Daniel Lv <lgn21st@gmail.com>
2014-05-21 21:56:36 +0800

    Refactor Account#payment_address method

--- app/models/account.rb
+++ app/models/account.rb
@@ -28,15 +28,7 @@ class Account < ActiveRecord::Base
   has_many :partial_trees
 
   def payment_address
-    if self.payment_addresses.empty?
-      self.gen_payment_address
-    end
-    self.payment_addresses.last
-  end
-
-  def gen_payment_address
-    address = CoinRPC[self.currency].getnewaddress("payment")
-    self.payment_addresses.create(address: address, currency: self.currency)
+    payment_addresses.last || payment_addresses.create(currency: self.currency)
   end
 
   def self.after(*names)
