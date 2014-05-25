82842fb8e603f2ee20a57d855d48b61fb15c736c
Daniel Lv <lgn21st@gmail.com>
2014-05-26 05:05:31 +0800

    Generate coin deposit address in background queue.

--- app/models/payment_address.rb
+++ app/models/payment_address.rb
@@ -2,7 +2,15 @@ class PaymentAddress < ActiveRecord::Base
   include Currencible
   belongs_to :account
 
+  after_commit :gen_address, on: :create
+
   has_many :transactions, class_name: 'PaymentTransaction', foreign_key: 'address', primary_key: 'address'
 
-  validates_uniqueness_of :address
+  validates_uniqueness_of :address, allow_nil: true
+
+  def gen_address
+    payload = { payment_address_id: id, currency: currency }
+    attrs = { persistent: true }
+    AMQPQueue.enqueue(:deposit_coin_address, payload, attrs)
+  end
 end
