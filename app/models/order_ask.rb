5a549b87e6efa07103ceacf2f89d4d5f823ebdfd
Jan Xie <jan.h.xie@gmail.com>
2014-05-30 21:49:34 +0800

    refactor method placement in OrderAsk and OrderBid

--- app/models/order_ask.rb
+++ app/models/order_ask.rb
@@ -1,5 +1,13 @@
 class OrderAsk < Order
 
+  has_many :trades, foreign_key: 'ask_id'
+
+  scope :matching_rule, -> { order('price ASC, created_at ASC') }
+
+  def self.strike_sum(volume, price)
+    [volume, volume * price]
+  end
+
   def hold_account
     member.get_account(ask)
   end
@@ -12,15 +20,8 @@ class OrderAsk < Order
     v ||= volume
   end
 
-  def self.strike_sum(volume, price)
-    [volume, volume * price]
-  end
-
   def hold_account_attr
     :origin_volume
   end
 
-  has_many :trades, foreign_key: 'ask_id'
-
-  scope :matching_rule, -> { order('price ASC, created_at ASC') }
 end
--- app/models/order_bid.rb
+++ app/models/order_bid.rb
@@ -1,5 +1,13 @@
 class OrderBid < Order
 
+  has_many :trades, foreign_key: 'bid_id'
+
+  scope :matching_rule, -> { order('price DESC, created_at ASC') }
+
+  def self.strike_sum(volume, price)
+    [volume * price, volume]
+  end
+
   def hold_account
     member.get_account(bid)
   end
@@ -14,15 +22,8 @@ class OrderBid < Order
     (v * p) if (v && p)
   end
 
-  def self.strike_sum(volume, price)
-    [volume * price, volume]
-  end
-
   def hold_account_attr
     :sum
   end
 
-  has_many :trades, foreign_key: 'bid_id'
-
-  scope :matching_rule, -> { order('price DESC, created_at ASC') }
 end
