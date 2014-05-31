b1493e23b2a9b3e5d410e704466ec3ab16a43532
Jan Xie <jan.h.xie@gmail.com>
2014-05-31 18:57:38 +0800

    estimate the funds required by market order

--- app/models/order_bid.rb
+++ app/models/order_bid.rb
@@ -26,13 +26,32 @@ class OrderBid < Order
     :sum
   end
 
+  LOCKING_BUFFER_FACTOR = '1.1'.to_d
   def compute_locked
     case ord_type
     when 'limit'
       price*volume
     when 'market'
-      '100'.to_d # TODO: replace stub
+      estimate_required_funds.mult_and_round(LOCKING_BUFFER_FACTOR)
     end
+  end
+
+  def estimate_required_funds
+    required_funds = Account::ZERO
+    expected_volume = volume
+
+    price_levels = Global[currency].asks
+
+    until expected_volume.zero? || price_levels.empty?
+      level_price, level_volume = price_levels.shift
+      v = [expected_volume, level_volume].min
+      required_funds += level_price.mult_and_round(v)
+      expected_volume -= v
+    end
+
+    raise "Market is not deep enough" unless expected_volume.zero?
+
+    required_funds
   end
 
 end
--- spec/models/order_bid_spec.rb
+++ spec/models/order_bid_spec.rb
@@ -6,4 +6,29 @@ describe OrderBid do
 
   its(:compute_locked) { should == subject.volume*subject.price }
 
+  context "#estimate_required_funds" do
+    let(:price_levels) do
+      [ ['1.0'.to_d, '10.0'.to_d],
+        ['2.0'.to_d, '20.0'.to_d],
+        ['3.0'.to_d, '30.0'.to_d] ]
+    end
+
+    before do
+      global = Global.new('btccny')
+      global.stubs(:asks).returns(price_levels)
+      Global.stubs(:[]).returns(global)
+    end
+
+    it "should require a little" do
+      OrderBid.new(volume: '5'.to_d).estimate_required_funds.should == '5'.to_d
+    end
+
+    it "should require more" do
+      OrderBid.new(volume: '35'.to_d).estimate_required_funds.should == '65'.to_d
+    end
+
+    it "should raise error if the market is not deep enough" do
+      expect { OrderBid.new(volume: '100'.to_d).estimate_required_funds }.to raise_error
+    end
+  end
 end
