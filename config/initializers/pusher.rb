5e337275937c16f68beaa80b6196b41c896172f8
Jan Xie <jan.h.xie@gmail.com>
2014-06-14 08:07:17 +0800

    fix pusher port

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,4 +2,4 @@ Pusher.app_id = ENV['PUSHER_APP']
 Pusher.key    = ENV['PUSHER_KEY']
 Pusher.secret = ENV['PUSHER_SECRET']
 Pusher.host   = ENV['PUSHER_HOST'] || 'api.pusherapp.com'
-Pusher.port   = ENV['PUSHER_PORT'].to_i || 80
+Pusher.port   = ENV['PUSHER_PORT'].present? ? ENV['PUSHER_PORT'].to_i : 80
