2b54feeb256ea91904248b291417a68945e91083
Daniel Lv <lgn21st@gmail.com>
2014-06-18 00:33:54 +0800

    Fix rspec and remove reset_two_factor related tests.

--- spec/factories/token.rb
+++ spec/factories/token.rb
@@ -2,9 +2,6 @@ FactoryGirl.define do
   factory :reset_password do
   end
 
-  factory :reset_two_factor do
-  end
-
   factory :activation do
   end
 
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -5,21 +5,13 @@ describe Token do
 
   it 'mark old tokens of the same type for the same identity as used' do
     reset_password = create :reset_password, member: member
-    reset_two_factor = create :reset_two_factor, member: member
-
     expect(reset_password.is_used).to be_false
-    expect(reset_two_factor.is_used).to be_false
 
     Timecop.travel(6.minutes.from_now)
 
     new_reset_password = create :reset_password, member: member
-    new_reset_two_factor = create :reset_two_factor, member: member
-
     expect(reset_password.reload.is_used).to be_true
-    expect(reset_two_factor.reload.is_used).to be_true
-
     expect(new_reset_password.is_used).to be_false
-    expect(new_reset_two_factor.is_used).to be_false
   end
 
   it 'dont mark old tokens of the too soon create new one' do
