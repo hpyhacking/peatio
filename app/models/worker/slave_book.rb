84f0882bc32d451670b7de6a42240d4d7f273e3d
Jan Xie <jan.h.xie@gmail.com>
2014-06-30 09:36:02 +0800

    log more debug info

--- app/models/worker/slave_book.rb
+++ app/models/worker/slave_book.rb
@@ -37,10 +37,13 @@ module Worker
 
     def cache_book
       @managers.keys.each do |market|
-        Rails.cache.write "peatio:#{market}:depth:asks", get_depth(market, :ask)
-        Rails.logger.debug "SlaveBook - asks depth updated."
-        Rails.cache.write "peatio:#{market}:depth:bids", get_depth(market, :bid)
-        Rails.logger.debug "SlaveBook - bids depth updated."
+        asks = get_depth(market, :ask)
+        Rails.cache.write "peatio:#{market}:depth:asks", asks
+        Rails.logger.debug "SlaveBook #{market} - asks depth updated (#{asks.first.last.to_s('F')}@#{asks.first.first.to_s('F')})"
+
+        bids = get_depth(market, :bid)
+        Rails.cache.write "peatio:#{market}:depth:bids", bids
+        Rails.logger.debug "SlaveBook #{market} - bids depth updated (#{bids.first.last.to_s('F')}@#{bids.first.first.to_s('F')})"
       end
     end
 
