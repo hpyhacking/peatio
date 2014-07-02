529d74098d4d47708e11732ddfda079bc77e284b
Jan Xie <jan.h.xie@gmail.com>
2014-07-02 18:28:38 +0800

    log slavebook cache thread

--- app/models/worker/slave_book.rb
+++ app/models/worker/slave_book.rb
@@ -39,6 +39,7 @@ module Worker
       @managers.keys.each do |market|
         Rails.cache.write "peatio:#{market}:depth:asks", get_depth(market, :ask)
         Rails.cache.write "peatio:#{market}:depth:bids", get_depth(market, :bid)
+        Rails.logger.debug "SlaveBook (#{market}) updated"
       end
     rescue
       Rails.logger.error "Failed to cache book: #{$!}"
