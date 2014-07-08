620a5f7d29d6da191c39719dc6f0a8cd50e297fd
Terry Tai <poshboytl@gmail.com>
2014-07-08 13:49:11 +0800

    Updated the annoate.

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: audit_logs
+#
+#  id             :integer          not null, primary key
+#  type           :string(255)
+#  operator_id    :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#  auditable_id   :integer
+#  auditable_type :string(255)
+#  source_state   :string(255)
+#  target_state   :string(255)
+#
+
 class AuditLog < ActiveRecord::Base
   belongs_to :operator, class_name: 'Member', foreign_key: 'operator_id'
   belongs_to :auditable, polymorphic: true
--- app/models/transfer_audit_log.rb
+++ app/models/transfer_audit_log.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: audit_logs
+#
+#  id             :integer          not null, primary key
+#  type           :string(255)
+#  operator_id    :integer
+#  created_at     :datetime
+#  updated_at     :datetime
+#  auditable_id   :integer
+#  auditable_type :string(255)
+#  source_state   :string(255)
+#  target_state   :string(255)
+#
+
 class TransferAuditLog < AuditLog
 
   def self.audit!(transfer, operator = nil)
