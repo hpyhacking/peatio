e6fa72d310ace7dbc3fe361cbabc8aca70b81511
Jan Xie <jan.h.xie@gmail.com>
2014-07-18 16:03:22 +0800

    refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ require 'whitelist_constraint'
 
 Rails.application.eager_load! if Rails.env.development?
 
+class ActionDispatch::Routing::Mapper
+  def draw(routes_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{routes_name}.rb")))
+  end
+end
+
 Peatio::Application.routes.draw do
 
   root 'welcome#index'
@@ -84,45 +90,7 @@ Peatio::Application.routes.draw do
 
   end
 
-  namespace :admin do
-    get '/', to: 'dashboard#index', as: :dashboard
-
-    resources :documents
-    resource :currency_deposit, :only => [:new, :create]
-    resources :proofs
-    resources :tickets, only: [:index, :show] do
-      member do
-        patch :close
-      end
-      resources :comments, only: [:create]
-    end
-
-    resources :members, :only => [:index, :show, :update] do
-      member do
-        post :toggle
-      end
-    end
-
-    namespace :deposits do
-      Deposit.descendants.each do |d|
-        resources d.resource_name
-      end
-    end
-
-    namespace :withdraws do
-      Withdraw.descendants.each do |w|
-        resources w.resource_name
-      end
-    end
-
-    namespace :statistic do
-      resource :members, :only => :show
-      resource :orders, :only => :show
-      resource :trades, :only => :show
-      resource :deposits, :only => :show
-      resource :withdraws, :only => :show
-    end
-  end
+  draw :admin
 
   constraints(WhitelistConstraint.new(JSON.parse(Figaro.env.try(:api_whitelist) || '[]'))) do
     namespace :api, defaults: {format: 'json'}, :constraints => MarketConstraint do
---
+++ config/routes/admin.rb
@@ -0,0 +1,39 @@
+namespace :admin do
+  get '/', to: 'dashboard#index', as: :dashboard
+
+  resources :documents
+  resource :currency_deposit, :only => [:new, :create]
+  resources :proofs
+  resources :tickets, only: [:index, :show] do
+    member do
+      patch :close
+    end
+    resources :comments, only: [:create]
+  end
+
+  resources :members, :only => [:index, :show, :update] do
+    member do
+      post :toggle
+    end
+  end
+
+  namespace :deposits do
+    Deposit.descendants.each do |d|
+      resources d.resource_name
+    end
+  end
+
+  namespace :withdraws do
+    Withdraw.descendants.each do |w|
+      resources w.resource_name
+    end
+  end
+
+  namespace :statistic do
+    resource :members, :only => :show
+    resource :orders, :only => :show
+    resource :trades, :only => :show
+    resource :deposits, :only => :show
+    resource :withdraws, :only => :show
+  end
+end
