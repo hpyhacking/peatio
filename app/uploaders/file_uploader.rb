2ab9b9bf2cb7a620c835026f5f1e3082e8e25406
Daniel Lv <lgn21st@gmail.com>
2014-07-20 21:29:34 +0800

    Using random and unique filename in id_document_file

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -3,7 +3,18 @@ class FileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png pdf)
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
