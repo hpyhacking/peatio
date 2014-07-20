2c280417cc50ff667bbba85acbdb07da7ac9e2b4
Daniel Lv <lgn21st@gmail.com>
2014-07-20 15:16:40 +0800

    Display asset image at backend

--- app/helpers/tag_helper.rb
+++ app/helpers/tag_helper.rb
@@ -3,4 +3,12 @@ module TagHelper
     raise unless MemberTag.tags.include?(key)
     content_tag('span', I18n.t("tags.#{key}"), :class => "member-tag #{key}")
   end
+
+  def admin_asset_tag(asset)
+    if asset.image?
+      link_to image_tag(asset.file.url, style: 'max-width:500px;max-height:500px;'), asset.file.url, target: '_blank'
+    else
+      link_to asset['file'], asset.file.url
+    end
+  end
 end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -13,6 +13,10 @@ class Asset < ActiveRecord::Base
   belongs_to :attachable, polymorphic: true
 
   mount_uploader :file, FileUploader
+
+  def image?
+    file.content_type.start_with?('image') if file?
+  end
 end
 
 class Asset::IdDocumentFile < Asset
