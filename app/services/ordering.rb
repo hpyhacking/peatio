5fdf69045e4769ac8d1fcaefa9502e9cb2f64040
Jan Xie <jan.h.xie@gmail.com>
2014-07-25 12:33:30 +0800

    make Ordering accept multiple orders

--- app/services/ordering.rb
+++ app/services/ordering.rb
@@ -3,63 +3,72 @@ class Ordering
   class LatestPriceError < RuntimeError; end
   class CancelOrderError < StandardError; end
 
-  def initialize(order)
-    @order = order
-  end
-
-  def member
-    @member ||= Member.find(@order.member_id)
+  def initialize(order_or_orders)
+    @orders = Array(order_or_orders)
   end
 
   def submit
-    check_price!
-
     ActiveRecord::Base.transaction do
-      @order.fix_number_precision # number must be fixed before computing locked
-      @order.locked = @order.origin_locked = @order.compute_locked
-      @order.save!
-
-      account = @order.hold_account
-      account.lock_funds(@order.locked, reason: Account::ORDER_SUBMIT, ref: @order)
-
+      @orders.each {|order| do_submit order }
     end
 
-    AMQPQueue.enqueue(:matching, action: 'submit', order: @order.to_matching_attributes)
+    @orders.each do |order|
+      AMQPQueue.enqueue(:matching, action: 'submit', order: order.to_matching_attributes)
+    end
 
     true
   end
 
   def cancel
-    AMQPQueue.enqueue(:matching, action: 'cancel', order: @order.to_matching_attributes)
+    @orders.each {|order| do_cancel order }
   end
 
   def cancel!
     ActiveRecord::Base.transaction do
-      order = Order.find(@order.id).lock!
-      account = @order.hold_account
-
-      if order.state == Order::WAIT
-        order.state = Order::CANCEL
-        account.unlock_funds(order.locked, reason: Account::ORDER_CANCEL, ref: order)
-        order.save!
-      else
-        raise CancelOrderError, "Only active order can be cancelled. id: #{order.id}, state: #{order.state}"
-      end
+      @orders.each {|order| do_cancel! order }
     end
   end
 
   private
 
-  def check_price!
-    if @order.ord_type == 'limit' && !price_in_range?
-      @order.errors.add(:price, :range)
+  def check_price!(order)
+    if order.ord_type == 'limit' && !price_in_range?(order)
+      order.errors.add(:price, :range)
       raise LatestPriceError, "invalid price"
     end
   end
 
-  def price_in_range?
-    latest = Trade.latest_price(@order.currency)
-    latest.zero? || PRICE_RANGE.cover?(@order.price / latest)
+  def price_in_range?(order)
+    latest = Trade.latest_price(order.currency)
+    latest.zero? || PRICE_RANGE.cover?(order.price / latest)
+  end
+
+  def do_submit(order)
+    check_price!(order)
+
+    order.fix_number_precision # number must be fixed before computing locked
+    order.locked = order.origin_locked = order.compute_locked
+    order.save!
+
+    account = order.hold_account
+    account.lock_funds(order.locked, reason: Account::ORDER_SUBMIT, ref: order)
+  end
+
+  def do_cancel(order)
+    AMQPQueue.enqueue(:matching, action: 'cancel', order: order.to_matching_attributes)
+  end
+
+  def do_cancel!(order)
+    account = order.hold_account
+    order   = Order.find(order.id).lock!
+
+    if order.state == Order::WAIT
+      order.state = Order::CANCEL
+      account.unlock_funds(order.locked, reason: Account::ORDER_CANCEL, ref: order)
+      order.save!
+    else
+      raise CancelOrderError, "Only active order can be cancelled. id: #{order.id}, state: #{order.state}"
+    end
   end
 
 end
