2aa47c2241dce65c8958c04b4f8c94814053b27b
Daniel Lv <lgn21st@gmail.com>
2014-07-31 18:43:32 +0800

    Refactor routing specs

--- spec/routing/private/two_factors_spec.rb
+++ spec/routing/private/two_factors_spec.rb
@@ -1,22 +1,21 @@
 require 'spec_helper'
 
-describe 'private' do
-
+describe 'two_factors' do
   describe 'get /two_factors/app' do
     it { expect(get('/two_factors/app')).to be_routable }
     it { expect(get('/two_factors/app')).to route_to \
-      controller: 'private/two_factors',
-      action: 'show',
-      id: 'app'
+         controller: 'private/two_factors',
+         action: 'show',
+         id: 'app'
     }
   end
 
   describe 'get /two_factors/app/edit' do
     it { expect(get('/two_factors/app/edit')).to be_routable }
     it { expect(get('/two_factors/app/edit')).to route_to \
-      controller: 'private/two_factors',
-      action: 'edit',
-      id: 'app'
+         controller: 'private/two_factors',
+         action: 'edit',
+         id: 'app'
     }
   end
 
