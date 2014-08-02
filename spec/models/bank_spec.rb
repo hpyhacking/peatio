7e0dce7c72ca300e28d258c803c13d60428890ce
Daniel Lv <lgn21st@gmail.com>
2014-08-03 03:38:37 +0800

    Fixed rspec tests

--- spec/models/bank_spec.rb
+++ spec/models/bank_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Bank do
   context '#with_currency' do
-    it { expect(Bank.with_currency(:usd)).not_to be_empty }
+    it { expect(Bank.with_currency(:cny)).not_to be_empty }
   end
 
   context '#currency_obj' do
-    subject { Bank.find_by_currency('usd') }
+    subject { Bank.with_currency(:cny).first }
     its(:currency_obj) { should be_present }
   end
 end
