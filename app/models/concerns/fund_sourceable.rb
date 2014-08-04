231946395b30f0e6e928d3f887f96229bf488e5e
Daniel Lv <lgn21st@gmail.com>
2014-08-05 01:18:16 +0800

    Refactor and rename FundSource#fund_extra to FundSource#fund_extra_text

--- app/models/concerns/fund_sourceable.rb
+++ app/models/concerns/fund_sourceable.rb
@@ -3,22 +3,22 @@ module FundSourceable
 
   included do
     attr_accessor :fund_source
-    before_validation :set_fund_extra_and_fund_uid, on: :create
+    before_validation :set_fund_source_attributes, on: :create
     validates :fund_source, presence: true, on: :create
   end
 
-  def set_fund_extra_and_fund_uid
+  def set_fund_source_attributes
     if fs = FundSource.find_by_id(fund_source)
       self.fund_extra = fs.extra
       self.fund_uid = fs.uid
     end
   end
 
-  def fund_extra
+  def fund_extra_text
     if self.currency_obj.coin?
-      self['fund_extra']
+      self.fund_extra
     else
-      I18n.t("banks.#{self['fund_extra']}")
+      I18n.t("banks.#{self.fund_extra}")
     end
   end
 end
