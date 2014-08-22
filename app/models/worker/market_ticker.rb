b915a4af62a2c991e279d02db0e07d5235485aaa
Jan Xie <jan.h.xie@gmail.com>
2014-08-22 12:04:57 +0800

    fix h24 low&high

--- app/models/worker/market_ticker.rb
+++ app/models/worker/market_ticker.rb
@@ -19,10 +19,11 @@ module Worker
     end
 
     def update_ticker(trade)
-      ticker = @tickers[trade.market.id]
-      ticker[:low]     = trade.price if trade.price < ticker[:low]
-      ticker[:high]    = trade.price if trade.price > ticker[:high]
-      ticker[:last]    = trade.price
+      ticker        = @tickers[trade.market.id]
+      ticker[:low]  = get_market_low trade.market.id, trade
+      ticker[:high] = get_market_high trade.market.id, trade
+      ticker[:last] = trade.price
+      Rails.logger.info ticker.inspect
       Rails.cache.write "peatio:#{trade.market.id}:ticker", ticker
     end
 
@@ -39,12 +40,52 @@ module Worker
       @trades[market.id] = trades.order('id desc').limit(FRESH_TRADES).map(&:for_global)
       Rails.cache.write "peatio:#{market.id}:trades", @trades[market.id]
 
+      if low_trade = trades.h24.order('price asc').first
+        ttl = low_trade.created_at.to_i + 24.hours - Time.now.to_i
+        write_h24_key "peatio:#{market.id}:h24:low", low_trade.price, ttl
+      end
+
+      if high_trade = trades.h24.order('price desc').first
+        ttl = high_trade.created_at.to_i + 24.hours - Time.now.to_i
+        write_h24_key "peatio:#{market.id}:h24:high", high_trade.price, ttl
+      end
+
       @tickers[market.id] = {
-        low:    trades.h24.minimum(:price) || ::Trade::ZERO,
-        high:   trades.h24.maximum(:price) || ::Trade::ZERO,
-        last:   trades.last.try(:price)    || ::Trade::ZERO
+        low:  low_trade.try(:price)   || ::Trade::ZERO,
+        high: high_trade.try(:price)  || ::Trade::ZERO,
+        last: trades.last.try(:price) || ::Trade::ZERO
       }
       Rails.cache.write "peatio:#{market.id}:ticker", @tickers[market.id]
+    end
+
+    private
+
+    def get_market_low(market, trade)
+      low_key = "peatio:#{market}:h24:low"
+      low = Rails.cache.read(low_key)
+
+      if low.nil? || trade.price < low
+        low = trade.price
+        write_h24_key low_key, low
+      end
+
+      low
+    end
+
+    def get_market_high(market, trade)
+      high_key = "peatio:#{market}:h24:high"
+      high = Rails.cache.read(high_key)
+
+      if high.nil? || trade.price > high
+        high = trade.price
+        write_h24_key high_key, high
+      end
+
+      high
+    end
+
+    def write_h24_key(key, value, ttl=24.hours)
+      Rails.cache.write key, value, expires_in: ttl
     end
 
   end
