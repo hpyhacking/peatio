98306cffbe6094f0d943990b9a77a11f3fe39bb8
Daniel Lv <lgn21st@gmail.com>
2014-08-30 18:03:51 +0800

    Remove member search functionality from statistic page

--- app/controllers/admin/statistic/members_controller.rb
+++ app/controllers/admin/statistic/members_controller.rb
@@ -2,13 +2,8 @@ module Admin
   module Statistic
     class MembersController < BaseController
       def show
-        @q = Member.order('id desc').includes(:two_factors, :id_document).search(params[:q])
-        result = @q.result(distinct: true)
-        @result_count = result.size
-        @members_count = Member.all.size
-        @members = result.page params[:page]
-
-        @register_group = result.where('created_at > ?', 30.days.ago).select('date(created_at) as date, count(id) as total, sum(activated IS TRUE) as total_activated').group('date(created_at)')
+        @members_count = Member.count
+        @register_group = Member.where('created_at > ?', 30.days.ago).select('date(created_at) as date, count(id) as total, sum(activated IS TRUE) as total_activated').group('date(created_at)')
       end
     end
   end
