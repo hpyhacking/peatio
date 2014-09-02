678501ab0b2af8e43730c80a5cce6c0e1c08802f
Jan Xie <jan.h.xie@gmail.com>
2014-09-02 12:40:10 +0800

    refactor omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,9 @@
-signup = lambda { |env| 
-  IdentitiesController.action(:new).call(env) 
-}
-
-failure = lambda { |env|
-  SessionsController.action(:failure).call(env)
-}
-
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity, fields: [:email], on_failed_registration: IdentitiesController.action(:new)
 end
 
-OmniAuth.config.on_failure = failure
+OmniAuth.config.on_failure = lambda do |env|
+  SessionsController.action(:failure).call(env)
+end
+
 OmniAuth.config.logger = Rails.logger
