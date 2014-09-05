6e37bfaf1e4e12f54cfb9500d9ecab3488fd4983
Jan Xie <jan.h.xie@gmail.com>
2014-09-05 13:16:16 +0800

    don't cache private pages

--- app/controllers/private/base_controller.rb
+++ app/controllers/private/base_controller.rb
@@ -1,5 +1,14 @@
 module Private
   class BaseController < ::ApplicationController
-    before_filter :auth_member!
+    before_filter :no_cache, :auth_member!
+
+    private
+
+    def no_cache
+      response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+      response.headers["Pragma"] = "no-cache"
+      response.headers["Expires"] = "Sat, 03 Jan 2009 00:00:00 GMT"
+    end
+
   end
 end
