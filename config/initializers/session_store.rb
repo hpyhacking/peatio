4b1656f5059a3be4bef79b273b14b69d9620c464
Jan Xie <jan.h.xie@gmail.com>
2014-09-05 14:10:42 +0800

    use redis store for session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Peatio::Application.config.session_store :cookie_store,
+Peatio::Application.config.session_store :redis_store,
   :key => '_peatio_session',
   :expire_after => ENV['SESSION_EXPIRE'].to_i.minutes
 
