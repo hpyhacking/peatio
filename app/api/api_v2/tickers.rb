0af3ad29cf6e3cb4c93d74a524c9bf18d944fcb6
Jan Xie <jan.h.xie@gmail.com>
2014-09-09 09:59:37 +0800

    enhancement #238: api for all tickers

--- app/api/api_v2/tickers.rb
+++ app/api/api_v2/tickers.rb
@@ -2,23 +2,20 @@ module APIv2
   class Tickers < Grape::API
     helpers ::APIv2::NamedParams
 
+    desc 'Get ticker of all markets.'
+    get "/tickers" do
+      Market.all.inject({}) do |h, m|
+        h[m.id] = format_ticker Global[m.id].ticker
+        h
+      end
+    end
+
     desc 'Get ticker of specific market.'
     params do
       use :market
     end
     get "/tickers/:market" do
-      ticker = Global[params[:market]].ticker
-
-      { at: ticker[:at],
-        ticker: {
-          buy: ticker[:buy],
-          sell: ticker[:sell],
-          low: ticker[:low],
-          high: ticker[:high],
-          last: ticker[:last],
-          vol: ticker[:volume]
-        }
-      }
+      format_ticker Global[params[:market]].ticker
     end
 
   end
