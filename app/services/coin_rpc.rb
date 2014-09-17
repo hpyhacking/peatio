c938627c698ba5be4573f045c815bddab907e0d7
Jan Xie <jan.h.xie@gmail.com>
2014-09-17 10:18:02 +0800

    refactor CoinRPC to support multiple protocols

--- app/services/coin_rpc.rb
+++ app/services/coin_rpc.rb
@@ -3,44 +3,58 @@ require 'uri'
 require 'json'
 
 class CoinRPC
+
+  class JSONRPCError < RuntimeError; end
+  class ConnectionRefusedError < StandardError; end
+
   def initialize(uri)
     @uri = URI.parse(uri)
   end
 
   def self.[](currency)
-    url = Currency.find_by_code(currency.to_s).rpc
-    url ? self.new(url) : nil
-  end
-
-  def safe_getbalance
-    begin
-      getbalance
-    rescue
-      'N/A'
+    c = Currency.find_by_code(currency.to_s)
+    if c && url = c.rpc
+      klass = "::CoinRPC::#{currency.upcase}".constantize
+      klass.new url
     end
   end
 
   def method_missing(name, *args)
-    post_body = { 'method' => name, 'params' => args, 'id' => 'jsonrpc' }.to_json
-    resp = JSON.parse( http_post_request(post_body) )
-    raise JSONRPCError, resp['error'] if resp['error']
-    result = resp['result']
-    result.symbolize_keys! if result.is_a? Hash
-    result
+    handle name, *args
   end
 
-  def http_post_request(post_body)
-    http    = Net::HTTP.new(@uri.host, @uri.port)
-    request = Net::HTTP::Post.new(@uri.request_uri)
-    request.basic_auth @uri.user, @uri.password
-    request.content_type = 'application/json'
-    request.body = post_body
-    http.request(request).body
-  rescue Errno::ECONNREFUSED => e
-    raise ConnectionRefusedError
+  def handle
+    raise "Not implemented"
   end
 
-  class JSONRPCError < RuntimeError; end
-  class ConnectionRefusedError < StandardError
+  class BTC < self
+    def handle(name, *args)
+      post_body = { 'method' => name, 'params' => args, 'id' => 'jsonrpc' }.to_json
+      resp = JSON.parse( http_post_request(post_body) )
+      raise JSONRPCError, resp['error'] if resp['error']
+      result = resp['result']
+      result.symbolize_keys! if result.is_a? Hash
+      result
+    end
+
+    def http_post_request(post_body)
+      http    = Net::HTTP.new(@uri.host, @uri.port)
+      request = Net::HTTP::Post.new(@uri.request_uri)
+      request.basic_auth @uri.user, @uri.password
+      request.content_type = 'application/json'
+      request.body = post_body
+      http.request(request).body
+    rescue Errno::ECONNREFUSED => e
+      raise ConnectionRefusedError
+    end
+
+    def safe_getbalance
+      begin
+        getbalance
+      rescue
+        'N/A'
+      end
+    end
   end
+
 end
