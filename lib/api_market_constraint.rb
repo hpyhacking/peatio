66328d0ca87fa2be89716b9c21bbaac225d6195d
Jan Xie <jan.h.xie@gmail.com>
2014-09-17 11:30:47 +0800

    fix base_unit/quote_unit in api market constraint

--- lib/api_market_constraint.rb
+++ lib/api_market_constraint.rb
@@ -20,8 +20,8 @@ class APIMarketConstraint
 
     if market = Market.find_by_id(id)
       request.path_parameters[:market] = id
-      request.path_parameters[:ask] = market.target_unit
-      request.path_parameters[:bid] = market.price_unit
+      request.path_parameters[:ask] = market.base_unit
+      request.path_parameters[:bid] = market.quote_unit
     else
       false
     end
