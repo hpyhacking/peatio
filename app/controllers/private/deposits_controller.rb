39844861f78c81f7edd2c7a80298284be841abac
Jan Xie <jan.h.xie@gmail.com>
2014-09-18 15:03:17 +0800

    temporarily hide btsx channels

--- app/controllers/private/deposits_controller.rb
+++ app/controllers/private/deposits_controller.rb
@@ -3,8 +3,9 @@ module Private
     before_action :auth_activated!
     before_action :auth_verified!
 
+    HIDDEN_CHANNEL = %w(btsx)
     def index
-      @deposits = DepositChannel.all.sort
+      @deposits = DepositChannel.all.select{|c| !HIDDEN_CHANNEL.include?(c.currency)}.sort
     end
 
   end
