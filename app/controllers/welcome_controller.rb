603535941bd6d20406c427b12571675f72c768b9
Daniel Lv <lgn21st@gmail.com>
2014-09-20 20:49:12 +0800

    New home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,7 @@
 class WelcomeController < ApplicationController
-  layout 'landing'
-
   def index
     if current_user
       redirect_to market_path(current_market) and return
     end
-
-    @btc_balance = Proof.current(:btc).try(:balance)
-    @cny_balance  = Proof.current(:cny).try(:balance)
   end
 end
