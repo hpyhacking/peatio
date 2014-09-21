c022bb73bba99da41ad6f79650176623bff1b768
Jan Xie <jan.h.xie@gmail.com>
2014-09-21 22:33:52 +0800

    display btsx deposit/withdraw on staging

--- app/controllers/private/deposits_controller.rb
+++ app/controllers/private/deposits_controller.rb
@@ -3,7 +3,12 @@ module Private
     before_action :auth_activated!
     before_action :auth_verified!
 
-    HIDDEN_CHANNEL = %w(btsx)
+    if ENV['URL_HOST'] == 'stg.peatio.com'
+      HIDDEN_CHANNEL = %w()
+    else
+      HIDDEN_CHANNEL = %w(btsx)
+    end
+
     def index
       @deposits = DepositChannel.all.select{|c| !HIDDEN_CHANNEL.include?(c.currency)}.sort
     end
