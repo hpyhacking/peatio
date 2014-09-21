b6e5031ae57d0935ebe6fcee4ff86c687d8616ca
Jan Xie <jan.h.xie@gmail.com>
2014-09-22 01:02:46 +0800

    btsx deposit/withdraw go online!

--- app/controllers/private/deposits_controller.rb
+++ app/controllers/private/deposits_controller.rb
@@ -3,14 +3,8 @@ module Private
     before_action :auth_activated!
     before_action :auth_verified!
 
-    if ENV['URL_HOST'] == 'stg.peatio.com'
-      HIDDEN_CHANNEL = %w()
-    else
-      HIDDEN_CHANNEL = %w(btsx)
-    end
-
     def index
-      @deposits = DepositChannel.all.select{|c| !HIDDEN_CHANNEL.include?(c.currency)}.sort
+      @deposits = DepositChannel.all.sort
     end
 
   end
--- app/controllers/private/withdraws_controller.rb
+++ app/controllers/private/withdraws_controller.rb
@@ -3,14 +3,8 @@ module Private
     before_action :auth_activated!
     before_action :auth_verified!
 
-    if ENV['URL_HOST'] == 'stg.peatio.com'
-      HIDDEN_CHANNEL = %w()
-    else
-      HIDDEN_CHANNEL = %w(btsx)
-    end
-
     def index
-      @channels = WithdrawChannel.all.select{|c| !HIDDEN_CHANNEL.include?(c.currency)}.sort
+      @channels = WithdrawChannel.all.sort
     end
 
   end
