47076fdeb7f2cc8fd7192bc3246f55455792a52b
Jan Xie <jan.h.xie@gmail.com>
2014-09-25 10:04:16 +0800

    wire up cancel all asks/bids buttons

--- app/controllers/private/order_bids_controller.rb
+++ app/controllers/private/order_bids_controller.rb
@@ -6,5 +6,12 @@ module Private
       @order = OrderBid.new(order_params(:order_bid))
       order_submit
     end
+
+    def clear
+      @orders = OrderBid.where(member_id: current_user.id).with_state(:wait)
+      @orders.each {|o| Ordering.new(o).cancel }
+      render status: 200, nothing: true
+    end
+
   end
 end
