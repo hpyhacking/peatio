c727573c1c58d2f4083233c014967db10fdbaf79
Daniel Lv <lgn21st@gmail.com>
2014-10-03 20:16:38 +0800

    Refactor emails notification of tickets system

--- app/mailers/token_mailer.rb
+++ app/mailers/token_mailer.rb
@@ -2,12 +2,12 @@ class TokenMailer < BaseMailer
 
   def reset_password(email, token)
     @token_url = edit_reset_password_url(token)
-    mail :to => email
+    mail to: email
   end
 
   def activation(email, token)
     @token_url = edit_activation_url token
-    mail :to => email
+    mail to: email
   end
 
 end
