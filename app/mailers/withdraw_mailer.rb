48b9708bb24ad7e61b6e59f9df91baa6c67d2f7f
Daniel Lv <lgn21st@gmail.com>
2014-10-03 21:57:39 +0800

    Refactor withdrawal mails with html template

--- app/mailers/withdraw_mailer.rb
+++ app/mailers/withdraw_mailer.rb
@@ -1,16 +1,28 @@
 class WithdrawMailer < BaseMailer
 
-  def withdraw_state(withdraw_id)
-    @withdraw = Withdraw.find withdraw_id
-    mail :to => @withdraw.member.email
+  def submitted(withdraw_id)
+    set_mail(withdraw_id)
   end
 
   def accepted(withdraw_id)
-    @withdraw = Withdraw.find withdraw_id
-    mail :to => @withdraw.member.email
+    set_mail(withdraw_id)
+  end
+
+  def processing(withdraw_id)
+    set_mail(withdraw_id)
   end
 
   def done(withdraw_id)
+    set_mail(withdraw_id)
+  end
+
+  def withdraw_state(withdraw_id)
+    set_mail(withdraw_id)
+  end
+
+  private
+
+  def set_mail(withdraw_id)
     @withdraw = Withdraw.find withdraw_id
     mail :to => @withdraw.member.email
   end
