af5c3f28be1b6cac9b594b48aa0f44c73ac1e843
Daniel Lv <lgn21st@gmail.com>
2014-10-04 21:26:12 +0800

    Tiny refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,8 +25,8 @@ class SessionsController < ApplicationController
         clear_failed_logins
         reset_session rescue nil
         session[:member_id] = @member.id
-        save_session_key current_user.id, cookies['_peatio_session']
-        MemberMailer.notify_signin(current_user.id).deliver if @member.activated?
+        save_session_key @member.id, cookies['_peatio_session']
+        MemberMailer.notify_signin(@member.id).deliver if @member.activated?
         redirect_to settings_path
       end
     else
