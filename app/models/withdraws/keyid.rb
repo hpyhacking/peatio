ad0dcbcb2e78594d2e1427436cbe25fdbd18fb1f
Jan Xie <jan.h.xie@gmail.com>
2014-10-05 17:51:44 +0800

    DNS withdraw scaffold

---
+++ app/models/withdraws/keyid.rb
@@ -0,0 +1,18 @@
+module Withdraws
+  class Keyid < ::Withdraw
+    include ::AasmAbsolutely
+    include ::Withdraws::Coinable
+    include ::FundSourceable
+
+    validates :sum, presence: true, numericality: {greater_than: 0.0001}, on: :create
+
+    def set_fee
+      self.fee = '0.1'.to_d
+    end
+
+    def sendtoaddress_args
+      [fund_uid, amount.to_f, memo]
+    end
+
+  end
+end
