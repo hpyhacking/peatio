f26e75b2d42b2bff55e0ceea2c905ca7fc6dca41
Jan Xie <jan.h.xie@gmail.com>
2014-10-05 19:51:04 +0800

    memo with checksum

--- app/models/payment_address.rb
+++ app/models/payment_address.rb
@@ -10,7 +10,7 @@ class PaymentAddress < ActiveRecord::Base
 
   def gen_address
     if account && %w(btsx dns).include?(account.currency)
-      self.address = "#{currency_obj.deposit_account}|#{account.id}"
+      self.address = "#{currency_obj.deposit_account}|#{construct_memo(account)}"
       save
     else
       payload = { payment_address_id: id, currency: currency }
@@ -18,4 +18,23 @@ class PaymentAddress < ActiveRecord::Base
       AMQPQueue.enqueue(:deposit_coin_address, payload, attrs)
     end
   end
+
+  def construct_memo(account)
+    member_id = account.member_id.to_s
+    size      = member_id.size.to_s(16).upcase
+    "#{member_id}#{account.id}#{size}"
+  end
+
+  def destruct_memo(memo)
+    size = memo.last.to_i(16)
+    return nil if size > (memo.size-2)
+
+    member_id = memo[0..(size-1)]
+    member = Member.find_by_id member_id
+    return nil unless member
+
+    account_id = memo[size..-2]
+    member.accounts.where(id: account_id).first
+  end
+
 end
--- spec/models/payment_address_spec.rb
+++ spec/models/payment_address_spec.rb
@@ -17,4 +17,29 @@ describe PaymentAddress do
     end
   end
 
+  context '#construct_memo' do
+    subject { PaymentAddress.new }
+
+    it "constructs memo" do
+      subject.construct_memo(Account.new(member_id: 60, id: 100)).should == '601002'
+      subject.construct_memo(Account.new(member_id: 12345678901, id: 12345678902)).should == '1234567890112345678902B'
+    end
+  end
+
+  context '#destruct_memo' do
+    let(:account) { create(:member).get_account('btc') }
+    let!(:memo)   { subject.construct_memo account }
+
+    subject { PaymentAddress.new }
+
+    it "returns the corresponding account if memo is valid" do
+      subject.destruct_memo(memo).should == account
+    end
+
+    it "returns nil if size bit is missing" do
+      wrong_memo = memo[0..-2]
+      subject.destruct_memo(wrong_memo).should be_nil
+    end
+  end
+
 end
