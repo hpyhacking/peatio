63ab9c5b3902d5a90a6d681968e5e356b6fa9182
Jan Xie <jan.h.xie@gmail.com>
2014-10-07 23:19:43 +0800

    add deposit_address
    Conflicts:
    	app/models/payment_address.rb

--- app/models/payment_address.rb
+++ app/models/payment_address.rb
@@ -17,6 +17,31 @@ class PaymentAddress < ActiveRecord::Base
     AMQPQueue.enqueue(:deposit_coin_address, payload, attrs)
   end
 
+  def memo
+    address && address.split('|', 2).last
+  end
+
+  def deposit_address
+    currency_obj.deposit_account || address
+  end
+
+  def self.construct_memo(obj)
+    member = obj.is_a?(Account) ? obj.member : obj
+    checksum = member.created_at.to_i.to_s[-3..-1]
+    "#{member.id}#{checksum}"
+  end
+
+  def self.destruct_memo(memo)
+    member_id = memo[0...-3]
+    checksum  = memo[-3..-1]
+
+    member = Member.find_by_id member_id
+    return nil unless member
+    return nil unless member.created_at.to_i.to_s[-3..-1] == checksum
+    member
+>>>>>>> 64ba8b1... add deposit_address
+  end
+
   def sync_create
     if self.address_changed?
       ::Pusher["private-#{account.member.sn}"].trigger_async('payment_address', { type: 'create', attributes: self.as_json})
