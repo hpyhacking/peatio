6d5a571de9c0a4919e16503c496bd2d80d388973
Jan Xie <jan.h.xie@gmail.com>
2014-10-09 14:30:46 +0800

    refactor account history page

--- app/helpers/private/history_helper.rb
+++ app/helpers/private/history_helper.rb
@@ -4,4 +4,14 @@ module Private::HistoryHelper
     trade.ask_member == current_user ? 'sell' : 'buy'
   end
 
+  def transaction_type(t)
+    t(".#{t.class.superclass.name}")
+  end
+
+  def transaction_txid_link(t)
+    return unless t.currency_obj.coin?
+    txid = t.txid || ''
+    link_to txid, t.blockchain_url
+  end
+
 end
