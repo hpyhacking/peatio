4332231e12e0c6f771551a40077cb6dc430498a7
Daniel Lv <lgn21st@gmail.com>
2014-10-11 16:33:23 +0800

    Set Google TFA requires input sms verify code

--- app/controllers/verify/google_auths_controller.rb
+++ app/controllers/verify/google_auths_controller.rb
@@ -4,6 +4,7 @@ module Verify
     before_action :find_google_auth
     before_action :google_auth_activated?,   only: [:show, :create]
     before_action :google_auth_inactivated?, only: [:edit, :destroy]
+    before_action :verify_sms_two_factor,    only: [:update]
 
     def show
       @google_auth.refresh if params[:refresh]
@@ -18,8 +19,7 @@ module Verify
         @google_auth.active!
         redirect_to settings_path, notice: t('.notice')
       else
-        flash[:alert] = t('.alert')
-        redirect_to verify_google_auth_path
+        redirect_to verify_google_auth_path, alert: t('.alert')
       end
     end
 
@@ -28,8 +28,7 @@ module Verify
         @google_auth.deactive!
         redirect_to settings_path, notice: t('.notice')
       else
-        flash[:alert] = t('.alert')
-        redirect_to edit_verify_google_auth_path
+        redirect_to edit_verify_google_auth_path, alert: t('.alert')
       end
     end
 
@@ -49,11 +48,17 @@ module Verify
     end
 
     def google_auth_activated?
-      redirect_to settings_path if @google_auth.activated?
+      redirect_to settings_path, notice: t('.already_activated') if @google_auth.activated?
     end
 
     def google_auth_inactivated?
-      redirect_to settings_path if not @google_auth.activated?
+      redirect_to settings_path, notice: t('.not_activated_yet') if not @google_auth.activated?
+    end
+
+    def verify_sms_two_factor
+      if current_user.sms_two_factor.activated? and !two_factor_auth_verified?
+        redirect_to verify_google_auth_path, alert: t('.sms_two_factor_error')
+      end
     end
   end
 end
