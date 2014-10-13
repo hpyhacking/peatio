4684f442363c82ee0097a8f43262b659313b4f0a
Daniel Lv <lgn21st@gmail.com>
2014-10-13 22:05:33 +0800

    Set the interval of pusher notification to 3 sec.
      for avoid exceeding the limitation of pusher services.

--- app/models/worker/slave_book.rb
+++ app/models/worker/slave_book.rb
@@ -7,7 +7,7 @@ module Worker
       if run_cache_thread
         cache_thread = Thread.new do
           loop do
-            sleep 1
+            sleep 3
             cache_book
           end
         end
