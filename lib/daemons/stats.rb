1eed19c08c939cb177c13db3dbfdff334cb99cbc
Jan Xie <jan.h.xie@gmail.com>
2014-10-14 17:06:17 +0800

    generate trade stats

---
+++ lib/daemons/stats.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+ENV["RAILS_ENV"] ||= "development"
+
+root = File.expand_path(File.dirname(__FILE__))
+root = File.dirname(root) until File.exists?(File.join(root, 'config'))
+Dir.chdir(root)
+
+require File.join(root, "config", "environment")
+
+Rails.logger = @logger = Logger.new STDOUT
+
+
+$running = true
+Signal.trap("TERM") do
+  $running = false
+end
+
+workers = []
+Market.all.each do |market|
+  workers << Worker::TradeStats.new(market)
+end
+
+while($running) do
+  workers.each do |worker|
+    worker.run
+  end
+
+  sleep 5
+end
