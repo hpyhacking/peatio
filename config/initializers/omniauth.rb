783440a06e611c396e2641dfe1b7339123d0424e
Terry Tai <poshboytl@gmail.com>
2014-10-14 15:12:57 +0800

    Disable the default login and register form of omniauth identity.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :identity, fields: [:email], on_failed_registration: IdentitiesController.action(:new)
+  provider :weibo, ENV['YUNBI_WEIBO_KEY'], ENV['YUNBI_WEIBO_SECRET']
 end
 
 OmniAuth.config.on_failure = lambda do |env|
@@ -7,3 +8,17 @@ OmniAuth.config.on_failure = lambda do |env|
 end
 
 OmniAuth.config.logger = Rails.logger
+
+module OmniAuth
+  module Strategies
+   class Identity
+     def request_phase
+       redirect '/signin'
+     end
+
+     def registration_form
+       redirect '/signup'
+     end
+   end
+ end
+end
