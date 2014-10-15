708a859bf31179ac3f4de95a6e589502dbba4c3e
Jan Xie <jan.h.xie@gmail.com>
2014-10-15 18:14:57 +0800

    better daily stats mail layout, comparison with the day before

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -3,6 +3,8 @@ class SystemMailer < BaseMailer
   default from: ENV["SYSTEM_MAIL_FROM"],
           to:   ENV["SYSTEM_MAIL_TO"]
 
+  layout 'mailers/system'
+
   def balance_warning(amount, balance)
     @amount = amount
     @balance = balance
@@ -16,8 +18,9 @@ class SystemMailer < BaseMailer
     mail subject: "Trade execute error: #{@error}"
   end
 
-  def daily_stats(ts, stats)
+  def daily_stats(ts, stats, base)
     @stats = stats
+    @base  = base
     from   = Time.at(ts)
     to     = Time.at(ts + 1.day - 1)
     mail subject: "Daily Summary (#{from} - #{to})"
