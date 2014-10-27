2689be46d9429a70ae082495c5ad88eb4295d20d
Daniel Lv <lgn21st@gmail.com>
2014-10-27 18:43:50 +0800

    Cleanup deprecated deposits and withdraws related codes.

--- app/controllers/private/withdraws/banks_controller.rb
+++
@@ -1,5 +0,0 @@
-module Private::Withdraws
-  class BanksController < ::Private::Withdraws::BaseController
-    include ::Withdraws::CtrlBankable
-  end
-end
--- app/controllers/private/withdraws/satoshis_controller.rb
+++
@@ -1,5 +0,0 @@
-module Private::Withdraws
-  class SatoshisController < ::Private::Withdraws::BaseController
-    include ::Withdraws::CtrlCoinable
-  end
-end
