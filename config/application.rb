68cc8891b788b00d3c2f37d6347e0a382cb30f59
Daniel Lv <lgn21st@gmail.com>
2014-11-02 10:39:53 +0800

    Remove active_resource/railtie from application.rb

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require File.expand_path('../boot', __FILE__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
