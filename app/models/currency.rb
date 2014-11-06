08b00e3d4690ba09df2040ba28855f4c31458ea5
Jan Xie <jan.h.xie@gmail.com>
2014-11-06 10:53:04 +0800

    invisible currency
    Conflicts:
    	config/currencies.yml.example

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,8 +2,19 @@ class Currency < ActiveYamlBase
   include International
   include ActiveHash::Associations
 
+  field :visible, default: true
+
+  self.singleton_class.send :alias_method, :all_with_invisible, :all
+  def self.all
+    all_with_invisible.select &:visible
+  end
+
+  def self.enumerize
+    all_with_invisible.inject({}) {|hash, i| hash[i.id.to_sym] = i.code; hash }
+  end
+
   def self.hash_codes
-    @codes ||= all.inject({}) {|memo, i| memo[i.code.to_sym] = i.id; memo}
+    @codes ||= all_with_invisible.inject({}) {|memo, i| memo[i.code.to_sym] = i.id; memo}
   end
 
   def self.codes
