642921116da1a70e6b5d6e13570c416241151954
Jan Xie <jan.h.xie@gmail.com>
2014-11-06 12:41:04 +0800

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105023306) do
+ActiveRecord::Schema.define(version: 20141105090746) do
 
   create_table "account_versions", force: true do |t|
     t.integer  "member_id"
@@ -48,13 +48,16 @@ ActiveRecord::Schema.define(version: 20141105023306) do
   add_index "accounts", ["member_id"], name: "index_accounts_on_member_id", using: :btree
 
   create_table "api_tokens", force: true do |t|
-    t.integer  "member_id",                  null: false
-    t.string   "access_key",      limit: 50, null: false
-    t.string   "secret_key",      limit: 50, null: false
+    t.integer  "member_id",                        null: false
+    t.string   "access_key",            limit: 50, null: false
+    t.string   "secret_key",            limit: 50, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "trusted_ip_list"
     t.string   "label"
+    t.integer  "oauth_access_token_id"
+    t.datetime "expire_at"
+    t.string   "scopes"
   end
 
   add_index "api_tokens", ["access_key"], name: "index_api_tokens_on_access_key", unique: true, using: :btree
