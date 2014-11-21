c55b8c4fa59aff42923be016461b7d7773cfea42
Daniel Lv <lgn21st@gmail.com>
2014-11-21 10:17:02 +0800

    Fixed exception when doc is not exists.

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -3,6 +3,11 @@ class DocumentsController < ApplicationController
   def show
     @doc = Document.find_by_key(params[:id])
 
+    if not @doc
+      redirect_to(request.referer || root_path)
+      return
+    end
+
     if @doc.is_auth and !current_user
       redirect_to root_path, alert: t('activations.new.login_required')
     end
