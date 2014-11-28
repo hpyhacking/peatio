b36672545a6b10cd4c40af207e4ff1711859f0c5
shooter <shooter.hero@gmail.com>
2014-11-28 16:16:18 +0800

    Deposits api add params currency

--- app/api/api_v2/deposits.rb
+++ app/api/api_v2/deposits.rb
@@ -5,10 +5,11 @@ module APIv2
     desc 'Get your deposits information'
     params do
       use :auth
+      requires :currency, type: String, values: Currency.all.map(&:code)
     end
     get "/deposits" do
       authenticate!
-      present current_user.deposits, with: APIv2::Entities::Deposit
+      present current_user.deposits.with_currency(params[:currency]), with: APIv2::Entities::Deposit
     end
   end
 end
--- app/api/api_v2/entities/deposit.rb
+++ app/api/api_v2/entities/deposit.rb
@@ -2,12 +2,13 @@ module APIv2
   module Entities
     class Deposit < Base
       expose :currency
-      expose :amount
+      expose :amount, format_with: :decimal
       expose :fee
+      expose :fund_uid
       expose :txid
-      expose :created_at
+      expose :created_at, format_with: :iso8601
       expose :memo
-      expose :done_at
+      expose :done_at, format_with: :iso8601
     end
   end
 end
