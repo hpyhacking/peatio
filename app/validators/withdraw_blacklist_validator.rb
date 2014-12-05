a64cca9e13d2cf1fa7251f228d0051890d7f586a
Terry Tai <poshboytl@gmail.com>
2014-12-06 05:07:00 +0800

    No need load the blacklist from the yml everytime when it checks.

--- app/validators/withdraw_blacklist_validator.rb
+++ app/validators/withdraw_blacklist_validator.rb
@@ -1,21 +1,9 @@
 class WithdrawBlacklistValidator < ActiveModel::Validator
 
   def validate(record)
-    if blacklist.keys.include?(record.currency) && blacklist[record.currency].include?(record.fund_uid)
+    if record.channel.blacklist && record.channel.blacklist.include?(record.fund_uid)
       record.errors[:fund_uid] << I18n.t('withdraws.invalid_address')
     end
   end
 
-  private
-
-  def blacklist
-    if @blacklist.nil?
-      @blacklist = {}
-      withdraw_channels= YAML.load_file(Rails.root.join('config', 'withdraw_channels.yml'))
-      withdraw_channels.select{|c| c["blacklist"]}.each do |wc|
-        @blacklist[wc["currency"]] = wc["blacklist"]
-      end
-    end
-    @blacklist
-  end
 end
