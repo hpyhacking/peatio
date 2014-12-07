322eddd7a74efbb7025dca4ce1ce7118e02a4287
Jan Xie <jan.h.xie@gmail.com>
2014-12-07 15:22:07 +0800

    fix specs

--- app/models/withdraw_channel.rb
+++ app/models/withdraw_channel.rb
@@ -3,7 +3,12 @@ class WithdrawChannel < ActiveYamlBase
   include HashCurrencible
   include International
 
+  def blacklist
+    self[:blacklist]
+  end
+
   def as_json(options = {})
     super(options)['attributes'].merge({resource_name: key.pluralize})
   end
+
 end
