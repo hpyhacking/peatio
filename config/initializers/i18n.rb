f6fb611e0da598b44d98aa803d6c9bfbfa626e2d
hpyhacking <crazyflapjack@gmail.com>
2014-12-23 01:03:57 +0800

    fixed CI

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,13 +1,15 @@
-module ActionView::Helpers::TranslationHelper
-  def t_with_default_brand(*args)
-    brand = I18n.t('brand', default: 'PEATIO')
-    if args.last.is_a?(Hash)
-      args[-1] = args.last.merge brand: brand
-    else
-      args << {brand: brand}
-    end
-    t_without_default_brand(*args)
-  end
-  alias_method_chain :t, :default_brand
-end
+module I18n
+  class<< self
+    def t_with_default_brand(*args)
+      brand = t_without_default_brand('brand', default: 'PEATIO')
 
+      if args.last.is_a?(Hash)
+        args[-1] = args.last.merge brand: brand
+      else
+        args << {brand: brand}
+      end
+      t_without_default_brand(*args)
+    end
+    alias_method_chain :t, :default_brand
+  end
+end
