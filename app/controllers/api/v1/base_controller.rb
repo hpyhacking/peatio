65e468d025da87834684b2a1e5e0c7dbdc7f8029
Daniel Lv <lgn21st@gmail.com>
2015-01-14 01:18:55 +0800

    Remove deprecated APIv1

--- app/controllers/api/v1/base_controller.rb
+++
@@ -1,8 +0,0 @@
-module API
-  module V1
-    class BaseController < ::ApplicationController
-      skip_before_filter :set_language, :setting_default
-    end
-  end
-end
-
--- app/controllers/api/v1/tickers_controller.rb
+++
@@ -1,9 +0,0 @@
-module API
-  module V1
-    class TickersController < BaseController
-      def show
-        @ticker = Global[params[:id]].ticker
-      end
-    end
-  end
-end
