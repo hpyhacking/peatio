26f8eb0872b4a79dc843b12e3d3422aadc1ca6ac
hpyhacking <crazyflapjack@gmail.com>
2015-01-21 14:07:44 +0800

    use table show currencies summary

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,8 @@ module Admin
     skip_load_and_authorize_resource
 
     def index
+      @daemon_statuses = Global.daemon_statuses
+      @currencies_summary = Currency.all.map(&:summary)
     end
   end
 end
