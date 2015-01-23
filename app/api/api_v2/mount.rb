d61f16b528c7385088bd1ebdb41d348afec7c041
Jan Xie <jan.h.xie@gmail.com>
2015-01-23 21:11:33 +0800

    fix api documentation

--- app/api/api_v2/mount.rb
+++ app/api/api_v2/mount.rb
@@ -34,7 +34,7 @@ module APIv2
     mount Trades
     mount K
 
-    base_path = Rails.env.production? ? "#{ENV['URL_SCHEMA']}://#{ENV['URL_HOST']}" : nil
+    base_path = Rails.env.production? ? "#{ENV['URL_SCHEMA']}://#{ENV['URL_HOST']}/#{PREFIX}" : PREFIX
     add_swagger_documentation base_path: base_path,
       mount_path: '/doc/swagger', api_version: 'v2',
       hide_documentation_path: true
