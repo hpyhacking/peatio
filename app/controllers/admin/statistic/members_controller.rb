744e43b7a563e8f3f5c72290d197387a7bcaa724
Terry Tai <poshboytl@gmail.com>
2015-01-30 14:17:49 +0800

    Updated the member statistic.

--- app/controllers/admin/statistic/members_controller.rb
+++ app/controllers/admin/statistic/members_controller.rb
@@ -3,7 +3,9 @@ module Admin
     class MembersController < BaseController
       def show
         @members_count = Member.count
-        @register_group = Member.where('created_at > ?', 30.days.ago).select('date(created_at) as date, count(id) as total, sum(activated IS TRUE) as total_activated').group('date(created_at)')
+        @register_group = Member.where('created_at > ?', 30.days.ago)
+          .select('date(created_at) as date, count(id) as total, sum(email_activated IS TRUE OR phone_number_activated IS TRUE) as total_activated')
+          .group('date(created_at)')
       end
     end
   end
