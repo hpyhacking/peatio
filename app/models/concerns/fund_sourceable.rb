650a5bd6938c3b89aae9f47642e9ad3b07178ee7
hpyhacking <crazyflapjack@gmail.com>
2015-09-26 00:43:05 +0800

    fixed #471 #461

--- app/models/concerns/fund_sourceable.rb
+++ app/models/concerns/fund_sourceable.rb
@@ -2,13 +2,13 @@ module FundSourceable
   extend ActiveSupport::Concern
 
   included do
-    attr_accessor :fund_source_id
+    attr_accessor :fund_source
     before_validation :set_fund_source_attributes, on: :create
-    validates :fund_source_id, presence: true, on: :create
+    validates :fund_source, presence: true, on: :create
   end
 
   def set_fund_source_attributes
-    if fs = FundSource.find_by(id: fund_source_id)
+    if fs = FundSource.find_by(id: fund_source)
       self.fund_extra = fs.extra
       self.fund_uid = fs.uid.strip
     end
