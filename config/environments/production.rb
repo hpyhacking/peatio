87c5cd099fa46ebd02ab009ed28f173937671c2e
scaatterp2 <scatterp2010@gmail.com>
2017-09-12 13:27:05 +0300

    Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Peatio::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = false
+  config.force_ssl = true
 
   # Set to :debug to see everything in the log.
   config.log_level = :info
