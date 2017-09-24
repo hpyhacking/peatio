8888feabce488620ed475da667d6122636a8cbe5
scaatterp2 <scatterp2010@gmail.com>
2017-09-24 14:03:43 +0300

    fixed config issue

--- app/api/api_v3/executor.rb
+++ app/api/api_v3/executor.rb
@@ -8,16 +8,19 @@ class MyHandler < EM::Connection
   end
 
 end
-
-server = FE::Server.new('127.0.0.1', 8095, MyHandler) do |conn|
-  conn.comp_id = 'MY_COMP_ID'
+def gogogo
+  puts "starting at: #{Time.now}"
+  server = FE::Server.new('127.0.0.1', 8095, MyHandler) do |conn|
+    conn.comp_id = 'MY_COMP_ID'
+  end
+  # This will also start an EventMachine reactor
+  server.run!
+  # This would be used inside an already-running reactor
+  EM.run do
+    server.start_server()
+  end
 end
-
-# This will also start an EventMachine reactor
-server.run!
-
-# This would be used inside an already-running reactor
-EM.run do
-  server.start_server
-end
+t1 = Thread.new{gogogo()}
+#t1.join
+puts "Ending at: #{Time.now}"
 
