c72e7245f959c76534dc523b270bd7a74c941d8a
Yaroslav Konoplov <eahome00@gmail.com>
2018-01-04 14:33:13 +0200

    Log to file in test environment (#218)

--- config/environments/shared/logger.rb
+++ config/environments/shared/logger.rb
@@ -1,9 +1,12 @@
 Rails.application.configure do
 
-  # We are always logging to STDOUT since this is the most appropriate way to get logs in Docker environment.
-  config.logger = ActiveSupport::Logger.new STDOUT, \
-    level:     :debug,
-    formatter: Logger::Formatter.new
+  # In non-test environments logging always goes to STDOUT since this is the most appropriate way
+  # to get logs in Docker environment.
+  unless Rails.env.test?
+    config.logger = ActiveSupport::Logger.new STDOUT, \
+      level:     :debug,
+      formatter: Logger::Formatter.new
+  end
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -11,6 +14,12 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # The following lines don't affect configuration, however they are needed to suppress warnings.
+  # The configuration variables below will be used in case config.logger hasn't been set yet.
+
+  # Use the lowest log level to ensure availability of diagnostic information
+  # when problems arise.
   config.log_level = :debug
+
+  # Use default logging formatter so that PID and timestamp are not suppressed.
+  config.log_formatter = Logger::Formatter.new
 end
