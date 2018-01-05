c1b7797198ecc2e53d1df89e694ca4a2199ce73c
Galya Fedorenko <f.g.i.mind@gmail.com>
2018-01-05 20:07:51 +0200

    Fix for document_translations table (#228)

--- db/migrate/20140428203350_add_desc_and_keyword_to_documents.rb
+++ db/migrate/20140428203350_add_desc_and_keyword_to_documents.rb
@@ -3,7 +3,9 @@ class AddDescAndKeywordToDocuments < ActiveRecord::Migration
     add_column :documents, :desc, :text
     add_column :documents, :keywords, :text
 
-    add_column :document_translations, :desc, :text
-    add_column :document_translations, :keywords, :text
+    if table_exists?(:document_translations)
+      add_column :document_translations, :desc, :text
+      add_column :document_translations, :keywords, :text
+    end
   end
 end
--- db/migrate/20180103185615_drop_documents.rb
+++ db/migrate/20180103185615_drop_documents.rb
@@ -1,6 +1,6 @@
 class DropDocuments < ActiveRecord::Migration
   def change
     drop_table :documents
-    drop_table :document_translations
+    drop_table :document_translations if table_exists?(:document_translations)
   end
 end
