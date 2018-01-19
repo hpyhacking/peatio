cc6b7bf5ed1d7868bae42990621c9ff9db0e9e55
Yaroslav Konoplov <eahome00@gmail.com>
2018-01-19 20:49:08 +0200

    Allow to customize appearance by ENV. (#321)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Use a different cache store in development.
   config.cache_store = :redis_store, ENV['REDIS_URL']
