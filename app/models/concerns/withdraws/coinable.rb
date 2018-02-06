49f891cf2cb34827dae1d063fcdfc9edd6f61cb8
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-02-06 14:37:03 +0200

    Added opportunity set value of withdraw fee in withdraw_channels.yml (#451) (#456)

--- app/models/concerns/withdraws/coinable.rb
+++ app/models/concerns/withdraws/coinable.rb
@@ -2,10 +2,6 @@ module Withdraws
   module Coinable
     extend ActiveSupport::Concern
 
-    def set_fee
-      self.fee = "0.0001".to_d
-    end
-
     def blockchain_url
       currency_obj.blockchain_url(txid)
     end
