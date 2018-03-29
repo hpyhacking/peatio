86bba5b5d948eabfa08170d2de25324b7f4159de
Yaroslav Konoplov <eahome00@gmail.com>
2018-03-29 20:23:20 +0300

    Display Currency#code instead of Currency#to_s at /admin/proofs (fixes #734). (#737)

--- app/grids/proofs_grid.rb
+++ app/grids/proofs_grid.rb
@@ -1,22 +1,15 @@
 class ProofsGrid
-
   include Datagrid
 
-  scope do
-    Proof.order('id desc')
-  end
+  scope { Proof.order(id: :desc) }
 
   filter(:id, :integer)
-  filter(:created_at, :date, :range => true)
+  filter(:created_at, :date, range: true)
 
   column(:id)
-  column(:currency)
+  column(:currency) { |p| p.currency.code.upcase }
   column(:balance)
   column(:sum)
-  column(:created_at) do |model|
-    model.created_at.to_date
-  end
-  column :actions, html: true, header: '' do |proof|
-    link_to I18n.t('actions.edit'), edit_admin_proof_path(proof)
-  end
+  column(:created_at) { |p| p.created_at.to_date }
+  column(:actions, html: true, header: '') { |proof| link_to I18n.t('actions.edit'), edit_admin_proof_path(proof) }
 end
