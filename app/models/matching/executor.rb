fae936b04ec2cbdfe67596468ef34a37f4d6168e
Yaroslav Konoplov <eahome00@gmail.com>
2018-05-09 15:11:38 +0300

    Fix trade execution errors (#1055)
    * Fix trade execution errors
    
    * Fix

--- app/models/matching/executor.rb
+++ app/models/matching/executor.rb
@@ -2,26 +2,34 @@ require_relative 'constants'
 
 module Matching
   class Executor
-
     def initialize(payload)
       @payload = payload
-      @market  = Market.find payload[:market_id]
-      @price   = BigDecimal.new payload[:strike_price]
-      @volume  = BigDecimal.new payload[:volume]
-      @funds   = BigDecimal.new payload[:funds]
+      @market  = Market.find(payload[:market_id])
+      @price   = payload[:strike_price].to_d
+      @volume  = payload[:volume].to_d
+      @funds   = payload[:funds].to_d
+    end
+
+    def execute
+      execute!
+    rescue TradeExecutionError => e
+      report_exception(e)
+      false
     end
 
     def execute!
-      retry_on_error(5) { create_trade_and_strike_orders }
+      create_trade_and_strike_orders
       publish_trade
       @trade
     end
 
-    private
+  private
 
     def valid?
       return false if @ask.ord_type == 'limit' && @ask.price > @price
       return false if @bid.ord_type == 'limit' && @bid.price < @price
+      return false unless @ask.state == Order::WAIT
+      return false unless @bid.state == Order::WAIT
       @funds > ZERO && [@ask.volume, @bid.volume].min >= @volume
     end
 
@@ -29,57 +37,42 @@ module Matching
       @price >= @market.latest_price ? 'up' : 'down'
     end
 
-    # in worst condition, the method will run 1+retry_count times then fail
-    def retry_on_error(retry_count, &block)
-      block.call
-    rescue ActiveRecord::StatementInvalid
-      # cope with "Mysql2::Error: Deadlock found ..." exception
-      if retry_count > 0
-        sleep 0.2
-        retry_count -= 1
-        puts "Retry trade execution (#{retry_count} retry left) .."
-        retry
-      else
-        puts "Failed to execute trade: #{@payload.inspect}"
-        raise $!
-      end
-    end
-
     def create_trade_and_strike_orders
       ActiveRecord::Base.transaction do
         @ask = OrderAsk.lock(true).find(@payload[:ask_id])
         @bid = OrderBid.lock(true).find(@payload[:bid_id])
 
-        raise TradeExecutionError.new({ask: @ask, bid: @bid, price: @price, volume: @volume, funds: @funds}) unless valid?
+        unless valid?
+          raise TradeExecutionError.new \
+            ask: @ask, bid: @bid, price: @price, volume: @volume, funds: @funds
+        end
 
-        @trade = Trade.create!(ask_id: @ask.id, ask_member_id: @ask.member_id,
-                               bid_id: @bid.id, bid_member_id: @bid.member_id,
-                               price: @price, volume: @volume, funds: @funds,
-                               market_id: @market.id, trend: trend)
+        @trade = Trade.create! \
+          ask_id:        @ask.id,
+          ask_member_id: @ask.member_id,
+          bid_id:        @bid.id,
+          bid_member_id: @bid.member_id,
+          price:         @price,
+          volume:        @volume,
+          funds:         @funds,
+          market_id:     @market.id,
+          trend:         trend
 
         @bid.strike @trade
         @ask.strike @trade
       end
-
-      # TODO: temporary fix, can be removed after pusher -> polling refactoring
-      if @trade.ask_member_id == @trade.bid_member_id
-        @ask.hold_account.reload.trigger
-        @bid.hold_account.reload.trigger
-      end
     end
 
     def publish_trade
-      AMQPQueue.publish(
-        :trade,
-        @trade.as_json,
-        { headers: {
-            market: @market.id,
+      @ask.hold_account.reload.trigger
+      @bid.hold_account.reload.trigger
+      AMQPQueue.publish :trade, @trade.as_json, {
+        headers: {
+            market:       @market.id,
             ask_member_id: @ask.member_id,
             bid_member_id: @bid.member_id
           }
         }
-      )
     end
-
   end
 end
--- app/models/worker/trade_executor.rb
+++ app/models/worker/trade_executor.rb
@@ -1,11 +1,7 @@
 module Worker
   class TradeExecutor
-
-    def process(payload, metadata, delivery_info)
-      payload.symbolize_keys!
-      ::Matching::Executor.new(payload).execute!
-    rescue
-      raise $!
+    def process(payload)
+      ::Matching::Executor.new(payload.symbolize_keys).execute
     end
   end
 end
