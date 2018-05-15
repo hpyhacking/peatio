6be4908e91a1d772e1155383072ae8396da2b78c
Yaroslav Konoplov <eahome00@gmail.com>
2018-05-15 15:52:48 +0300

    Drop config/initializers/abstract_mysql2_adapte.rb (#1102)

--- config/initializers/abstract_mysql2_adapter.rb
+++
@@ -1,8 +0,0 @@
-# encoding: UTF-8
-# frozen_string_literal: true
-
-require 'active_record/connection_adapters/mysql2_adapter'
-
-class ActiveRecord::ConnectionAdapters::Mysql2Adapter
-  NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
-end
