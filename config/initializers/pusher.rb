f62b9427fb838fe928e74b2228dba755ecd6c2e8
Graham Pengelly <graham@three-tier.com>
2018-05-24 10:49:41 +0100

    Add PUSHER_CLUSTER (#1183)
    Add support for configuring pusher with `Pusher.cluster` via the
    PUSHER_CLUSTER environment variable. This is required by pusher.

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,9 +1,10 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
-Pusher.app_id = ENV.fetch('PUSHER_APP')
-Pusher.key    = ENV.fetch('PUSHER_CLIENT_KEY')
-Pusher.secret = ENV.fetch('PUSHER_SECRET')
-Pusher.host   = ENV.fetch('PUSHER_HOST')
-Pusher.scheme = ENV.fetch('PUSHER_SCHEME')
-Pusher.port   = ENV.fetch('PUSHER_PORT')
+Pusher.app_id  = ENV.fetch('PUSHER_APP')
+Pusher.key     = ENV.fetch('PUSHER_CLIENT_KEY')
+Pusher.secret  = ENV.fetch('PUSHER_SECRET')
+Pusher.host    = ENV.fetch('PUSHER_HOST')
+Pusher.scheme  = ENV.fetch('PUSHER_SCHEME')
+Pusher.port    = ENV.fetch('PUSHER_PORT')
+Pusher.cluster = ENV.fetch('PUSHER_CLUSTER')
