5f1eb6508453c7983ce48e2e59fd78f06b1df290
Dinesh Chohda <35770481+dinesh-skyach@users.noreply.github.com>
2018-05-31 01:04:22 +0530

    Bring back legacy Peatio benchmarks (closes #1189) (#1202)
    * Bring back legacy Peatio benchmark
    
    * Added uniq emails for users
    
    * Bring back legacy Peatio benchmark
    
    * Added uniq emails for users
    
    * Optimized make order queries

---
+++ lib/benchmark/sweat_factory.rb
@@ -0,0 +1,35 @@
+
+module Benchmark
+  class SweatFactory
+
+    class <<self
+      def make_member
+        member = Member.create!(
+          email: Faker::Internet.unique.email
+        )
+      end
+
+      def make_order(klass, attrs={})
+        klass.new({
+          bid: fiat_currency.id,
+          ask: coin_currency.id,
+          state: Order::WAIT,
+          market_id: "btc#{fiat_currency.code}".to_sym,
+          origin_volume: attrs[:volume],
+          ord_type: "limit"
+        }.merge(attrs))
+      end
+
+      private
+
+      def fiat_currency
+        @fiat_currency ||= Currency.fiats.first
+      end
+
+      def coin_currency
+        @coin_currency ||= Currency.coins.first
+      end
+    end
+
+  end
+end
