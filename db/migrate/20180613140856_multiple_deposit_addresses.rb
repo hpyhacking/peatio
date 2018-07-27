5cf6c4fc9f0be772f199edce09f8f0d04552c20b
Camille <cmeulien@heliostech.fr>
2018-07-27 20:00:23 +0200

    Fix migration multiple_deposit_addresses (#1402)

--- db/migrate/20180613140856_multiple_deposit_addresses.rb
+++ db/migrate/20180613140856_multiple_deposit_addresses.rb
@@ -12,8 +12,8 @@ class MultipleDepositAddresses < ActiveRecord::Migration
       Currency.where(type: :coin).find_each do |ccy|
         unless ccy.options.key?('supports_hd_protocol')
           ccy.options['supports_hd_protocol'] = \
-            ccy.code.in?(%w[btc btcd bch bchd ltc ltcd dash dashd]) ||
-            (ccy.code.in?(%w[xrp eth]) && ccy.options['api_client'] == 'BitGo')
+            ccy.id.in?(%w[btc btcd bch bchd ltc ltcd dash dashd]) ||
+            (ccy.id.in?(%w[xrp eth]) && ccy.options['api_client'] == 'BitGo')
         end
 
         unless ccy.options.key?('allow_multiple_deposit_addresses')
