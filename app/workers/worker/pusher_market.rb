bcb734fec351d65ba7661648595d451253be63e2
Egor Kovetskiy <e.kovetskiy@gmail.com>
2018-08-24 12:11:52 +0300

    replace PusherMarket and PusherMember with Peatio::MQ::Events

--- app/workers/worker/pusher_market.rb
+++ app/workers/worker/pusher_market.rb
@@ -1,13 +1,16 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
+require "peatio/mq/events"
+
 module Worker
   class PusherMarket
     def process(payload)
       trade = Trade.new(payload)
-      Pusher["private-#{trade.ask.member.sn}"].trigger(:trade, trade.for_notify('ask'))
-      Pusher["private-#{trade.bid.member.sn}"].trigger(:trade, trade.for_notify('bid'))
-      Pusher["market-#{trade.market.id}-global"].trigger(:trades, trades: [trade.for_global])
+
+      Peatio::MQ::Events.publish("private", trade.ask.member.sn, "trade", trade.for_notify("ask"))
+      Peatio::MQ::Events.publish("private", trade.bid.member.sn, "trade", trade.for_notify("bid"))
+      Peatio::MQ::Events.publish("public", trade.market.id, "trades", {trades: [trade.for_global]})
     end
   end
 end
--- app/workers/worker/pusher_member.rb
+++ app/workers/worker/pusher_member.rb
@@ -1,13 +1,17 @@
 # encoding: UTF-8
 # frozen_string_literal: true
 
+require "peatio/mq/events"
+
 module Worker
   class PusherMember
     def process(payload)
-      return unless (sn = Member.where(id: payload['member_id']).pluck(:sn).first)
-      event = payload['event']
-      data  = payload['data']
-      Pusher["private-#{sn}"].trigger(event, data)
+      return unless (sn = Member.where(id: payload["member_id"]).pluck(:sn).first)
+
+      event = payload["event"]
+      data = payload["data"]
+
+      Peatio::MQ::Events.publish("private", sn, event, data)
     end
   end
 end
