23b97a4346d6e53db446c0bfa6770421b09d47b4
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2018-12-13 15:55:44 +0200

    Extend entities specs

--- spec/api/v2/management/entities/operation_spec.rb
+++ spec/api/v2/management/entities/operation_spec.rb
@@ -2,27 +2,30 @@
 # frozen_string_literal: true
 
 describe API::V2::Management::Entities::Operation do
-  context 'asset' do
-    let(:record) { create(:asset) }
+  Operation::PLATFORM_TYPES.each do |op_type|
+    context op_type do
+      let(:record) { create(op_type) }
 
-    subject { OpenStruct.new API::V2::Management::Entities::Operation.represent(record).serializable_hash }
+      subject { OpenStruct.new API::V2::Management::Entities::Operation.represent(record).serializable_hash }
 
-    it { expect(subject.code).to eq record.code }
-    it { expect(subject.currency).to eq record.currency_id }
-    it { expect(subject.created_at).to eq record.created_at.iso8601 }
-    it { expect(subject.respond_to?(:uid)).to be_falsey }
+      it { expect(subject.code).to eq record.code }
+      it { expect(subject.currency).to eq record.currency_id }
+      it { expect(subject.created_at).to eq record.created_at.iso8601 }
+      it { expect(subject.respond_to?(:uid)).to be_falsey }
 
-    context 'credit' do
-      it { expect(subject.credit).to eq record.credit }
-      it { expect(subject.respond_to?(:debit)).to be_falsey }
-    end
+      context 'credit' do
+        it { expect(subject.credit).to eq record.credit }
+        it { expect(subject.respond_to?(:debit)).to be_falsey }
+      end
 
-    context 'credit' do
-      let(:record) { create(:asset, :debit) }
-      it { expect(subject.debit).to eq record.debit }
-      it { expect(subject.respond_to?(:credit)).to be_falsey }
+      context 'credit' do
+        let(:record) { create(:asset, :debit) }
+        it { expect(subject.debit).to eq record.debit }
+        it { expect(subject.respond_to?(:credit)).to be_falsey }
+      end
     end
   end
+
 
   context 'liability' do
     let(:record) { create(:liability) }
