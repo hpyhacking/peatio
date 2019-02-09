c4e5c97deb058c1b6eb2a9d3a198b3047c95b414
mnaichuk <mnaichuk@heliostech.fr>
2019-02-09 14:08:29 +0200

    Fix for_notify in pusher_market daemon

--- app/workers/worker/pusher_market.rb
+++ app/workers/worker/pusher_market.rb
@@ -8,8 +8,8 @@ module Worker
     def process(payload)
       trade = Trade.new(payload)
 
-      Peatio::MQ::Events.publish("private", trade.ask.member.uid, "trade", trade.for_notify(trade.ask.member.uid))
-      Peatio::MQ::Events.publish("private", trade.bid.member.uid, "trade", trade.for_notify(trade.bid.member.uid))
+      Peatio::MQ::Events.publish("private", trade.ask.member.uid, "trade", trade.for_notify(trade.ask.member))
+      Peatio::MQ::Events.publish("private", trade.bid.member.uid, "trade", trade.for_notify(trade.bid.member))
       Peatio::MQ::Events.publish("public", trade.market.id, "trades", {trades: [trade.for_global]})
     end
   end
