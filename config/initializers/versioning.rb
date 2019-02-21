a8f1fb9db49a4617d509bfbff0c806637ced8d7e
OpenWare <support@openware.com>
2019-02-21 17:38:37 +0000

    v2.0.32-alpha [skip ci]

--- config/initializers/versioning.rb
+++ config/initializers/versioning.rb
@@ -5,9 +5,9 @@
 
 module Peatio
   class Application
-    GIT_TAG =    '2.0.31-alpha'
-    GIT_SHA =    '913718b'
-    BUILD_DATE = '2019-02-21 14:28:15+00:00'
+    GIT_TAG =    '2.0.32-alpha'
+    GIT_SHA =    '26fa195'
+    BUILD_DATE = '2019-02-21 17:38:09+00:00'
     VERSION =    GIT_TAG
   end
 end
