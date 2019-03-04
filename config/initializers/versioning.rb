4036a1f67595fe047fddf9b52f589702622433cd
OpenWare <support@openware.com>
2019-03-04 13:24:12 +0000

    v2.1.5 [skip ci]

--- config/initializers/versioning.rb
+++ config/initializers/versioning.rb
@@ -5,9 +5,9 @@
 
 module Peatio
   class Application
-    GIT_TAG =    '2.1.4'
-    GIT_SHA =    '0cd3b0d'
-    BUILD_DATE = '2019-02-26 18:54:01+00:00'
+    GIT_TAG =    '2.1.5'
+    GIT_SHA =    '20da971'
+    BUILD_DATE = '2019-03-04 13:23:44+00:00'
     VERSION =    GIT_TAG
   end
 end
--- docs/api/peatio_management_api_v2.md
+++ docs/api/peatio_management_api_v2.md
@@ -1,7 +1,7 @@
 # Peatio Management API v2
 Management API is server-to-server API with high privileges.
 
-## Version: 2.1.4
+## Version: 2.1.5
 
 **Contact information:**  
 peatio.tech  
