6ed8be3bc0ee8fe52c377d2dc6bfd02ffe1040cc
OpenWare <support@openware.com>
2019-06-21 09:45:47 +0000

    v2.2.11 [skip ci]

--- config/initializers/versioning.rb
+++ config/initializers/versioning.rb
@@ -5,9 +5,9 @@
 
 module Peatio
   class Application
-    GIT_TAG =    '2.2.10'
-    GIT_SHA =    'b83d53d'
-    BUILD_DATE = '2019-06-20 14:10:39+00:00'
+    GIT_TAG =    '2.2.11'
+    GIT_SHA =    '0fdce26'
+    BUILD_DATE = '2019-06-21 09:45:19+00:00'
     VERSION =    GIT_TAG
   end
 end
--- docs/api/peatio_user_api_v2.md
+++ docs/api/peatio_user_api_v2.md
@@ -1,7 +1,7 @@
 # Peatio User API v2
 API for Peatio application.
 
-## Version: 2.2.10
+## Version: 2.2.11
 
 **Contact information:**  
 peatio.tech  
