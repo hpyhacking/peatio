1a2ac1e7cc104cf24647610f9972b68f7020c7d8
MoD <mod@websys.io>
2019-08-16 03:08:04 +0300

    Feature scale puma to 4 workers

--- config/puma.rb
+++ config/puma.rb
@@ -6,8 +6,10 @@
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-threads threads_count, threads_count
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { 2 }
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 8 }
+
+threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -23,14 +25,14 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("RAILS_PUMA_WORKERS") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
