49359ce2c68c733ad70b47eddc01159e1928e90c
Maksym <mnaichuk@heliostech.fr>
2019-08-16 14:08:07 +0300

    Use update_attribute in migrations (#2324)

--- db/migrate/20190711114027_update_markets_min_amount_and_price.rb
+++ db/migrate/20190711114027_update_markets_min_amount_and_price.rb
@@ -3,12 +3,13 @@ class UpdateMarketsMinAmountAndPrice < ActiveRecord::Migration[5.2]
     Market.find_each do |m|
       # Set price and amount precision to max possible if precisions sum greater
       # then FUNDS_PRECISION.
+      attributes = {}
       if m.amount_precision + m.price_precision > Market::FUNDS_PRECISION
-        m.price_precision = m.amount_precision = 6
+        attributes.merge!(price_precision: 6, amount_precision: 6)
       end
-      m.min_amount = m.min_amount_by_precision
-      m.min_price = m.min_price_by_precision
-      m.save!
+      attributes.merge!(min_amount: m.min_amount_by_precision,
+                        min_price: m.min_price_by_precision)
+      m.update_attributes(attributes)
     end
   end
 end
