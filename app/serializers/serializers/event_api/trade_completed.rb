736066c0f35fbabad2ecd7575c3a51d6ae4aa9f6
Maksym <mnaichuk@heliostech.fr>
2019-12-26 12:55:39 +0200

    Fix event api message on trade_completed (#2430)

--- app/serializers/serializers/event_api/trade_completed.rb
+++ app/serializers/serializers/event_api/trade_completed.rb
@@ -6,30 +6,65 @@ module Serializers
     class TradeCompleted
       def call(trade)
         {
-          id:                    trade.id,
-          market:                trade.market.id,
-          price:                 trade.price.to_s('F'),
-          maker_uid:             trade.maker.uid,
-          maker_income_unit:     trade.market.base_unit,
-          maker_income_amount:   trade.amount.to_s('F'),
-          maker_income_fee:      (trade.amount * trade.order_fee(trade.maker_order)).to_s('F'),
-          maker_outcome_unit:    trade.market.quote_unit,
-          maker_outcome_amount:  trade.total.to_s('F'),
-          maker_outcome_fee:     '0.0',
-          taker_uid:             trade.taker.uid,
-          taker_income_unit:     trade.market.quote_unit,
-          taker_income_amount:   trade.total.to_s('F'),
-          taker_income_fee:      (trade.total * trade.order_fee(trade.taker_order)).to_s('F'),
-          taker_outcome_unit:    trade.market.base_unit,
-          taker_outcome_amount:  trade.amount.to_s('F'),
-          taker_outcome_fee:     '0.0',
-          completed_at:          trade.created_at.iso8601
-        }
+          id: trade.id,
+          market: trade.market.id,
+          price: trade.price.to_s('F'),
+          amount: trade.amount.to_s('F'),
+          completed_at: trade.created_at.iso8601
+        }.merge(maker_attributes(trade), taker_attributes(trade))
       end
 
       class << self
         def call(trade)
           new.call(trade)
+        end
+      end
+
+      def maker_attributes(trade)
+        if trade.maker_order.side == 'sell'
+          {
+            maker_uid: trade.maker.uid,
+            maker_income_unit: trade.market.quote_unit,
+            maker_income_amount: trade.total.to_s('F'),
+            maker_income_fee: (trade.total * trade.order_fee(trade.maker_order)).to_s('F'),
+            maker_outcome_unit: trade.market.base_unit,
+            maker_outcome_amount: trade.amount.to_s('F'),
+            maker_outcome_fee: '0.0'
+          }
+        else
+          {
+            maker_uid: trade.maker.uid,
+            maker_income_unit: trade.market.base_unit,
+            maker_income_amount: trade.amount.to_s('F'),
+            maker_income_fee: (trade.amount * trade.order_fee(trade.maker_order)).to_s('F'),
+            maker_outcome_unit: trade.market.quote_unit,
+            maker_outcome_amount: trade.total.to_s('F'),
+            maker_outcome_fee: '0.0'
+          }
+        end
+      end
+
+      def taker_attributes(trade)
+        if trade.maker_order.side == 'sell'
+          {
+            taker_uid: trade.taker.uid,
+            taker_income_unit: trade.market.base_unit,
+            taker_income_amount: trade.amount.to_s('F'),
+            taker_income_fee: (trade.amount * trade.order_fee(trade.taker_order)).to_s('F'),
+            taker_outcome_unit: trade.market.quote_unit,
+            taker_outcome_amount: trade.total.to_s('F'),
+            taker_outcome_fee: '0.0'
+          }
+        else
+          {
+            taker_uid: trade.taker.uid,
+            taker_income_unit: trade.market.quote_unit,
+            taker_income_amount: trade.total.to_s('F'),
+            taker_income_fee: (trade.total * trade.order_fee(trade.taker_order)).to_s('F'),
+            taker_outcome_unit: trade.market.base_unit,
+            taker_outcome_amount: trade.amount.to_s('F'),
+            taker_outcome_fee: '0.0'
+          }
         end
       end
     end
--- spec/serializers/serializers/event_api/trade_completed_spec.rb
+++ spec/serializers/serializers/event_api/trade_completed_spec.rb
@@ -6,7 +6,7 @@ describe Serializers::EventAPI::TradeCompleted, 'Event API' do
 
   let(:taker) { create(:member, :level_3, :barong) }
 
-  let :order_ask do
+  let :order_ask_maker do
     create :order_ask, \
       bid:           :usd,
       ask:           :btc,
@@ -21,7 +21,37 @@ describe Serializers::EventAPI::TradeCompleted, 'Event API' do
       member:        maker
   end
 
-  let :order_bid do
+  let :order_ask_taker do
+    create :order_ask, \
+      bid:           :usd,
+      ask:           :btc,
+      market:        Market.find(:btcusd),
+      state:         :wait,
+      ord_type:      :limit,
+      price:         '0.03'.to_d,
+      volume:        '100.0',
+      origin_volume: '100.0',
+      locked:        '100.0',
+      origin_locked: '100.0',
+      member:        taker
+  end
+
+  let :order_bid_maker do
+    create :order_bid, \
+      bid:           :usd,
+      ask:           :btc,
+      market:        Market.find(:btcusd),
+      state:         :wait,
+      ord_type:      :limit,
+      price:         '0.03'.to_d,
+      volume:        '14.0',
+      origin_volume: '14.0',
+      locked:        '0.42',
+      origin_locked: '0.42',
+      member:        maker
+  end
+
+  let :order_bid_taker do
     create :order_bid, \
       bid:           :usd,
       ask:           :btc,
@@ -38,69 +68,141 @@ describe Serializers::EventAPI::TradeCompleted, 'Event API' do
 
   let(:completed_at) { Time.current }
 
-  let :executor do
-    ask = Matching::LimitOrder.new(order_ask.to_matching_attributes)
-    bid = Matching::LimitOrder.new(order_bid.to_matching_attributes)
-    Matching::Executor.new \
-      action: 'execute',
-      trade: {
-        market_id:    :btcusd,
-        maker_order_id:       ask.id,
-        taker_order_id:       bid.id,
-        strike_price: '0.03',
-        amount:       '14.0',
-        total:        '0.42'
-      }
+  context 'maker sell order' do
+    let :executor do
+      ask = Matching::LimitOrder.new(order_ask_maker.to_matching_attributes)
+      bid = Matching::LimitOrder.new(order_bid_taker.to_matching_attributes)
+      Matching::Executor.new \
+        action: 'execute',
+        trade: {
+          market_id:    :btcusd,
+          maker_order_id:       ask.id,
+          taker_order_id:       bid.id,
+          strike_price: '0.03',
+          amount:       '14.0',
+          total:        '0.42'
+        }
+    end
+
+    subject { executor.execute! }
+
+    before do
+      maker.ac(:btc).plus_funds('100.0'.to_d)
+      maker.ac(:btc).lock_funds('100.0'.to_d)
+    end
+
+    before do
+      taker.ac(:usd).plus_funds('100.0'.to_d)
+      taker.ac(:usd).lock_funds('14.0'.to_d)
+    end
+
+    before { Trade.any_instance.expects(:created_at).returns(completed_at).at_least_once }
+
+    before do
+      DatabaseCleaner.clean
+      EventAPI.expects(:notify).with('market.btcusd.order_created', anything).twice
+      EventAPI.expects(:notify).with('market.btcusd.order_updated', anything).once
+      EventAPI.expects(:notify).with('market.btcusd.order_completed', anything).once
+      EventAPI.expects(:notify).with('market.btcusd.trade_completed', {
+        id:                     1,
+        market:                 'btcusd',
+        price:                  '0.03',
+        amount:                 '14.0',
+        maker_uid:              maker.uid,
+        maker_income_unit:      'usd',
+        maker_income_amount:    '0.42',
+        maker_income_fee:       '0.00063',
+        maker_outcome_unit:     'btc',
+        maker_outcome_amount:   '14.0',
+        maker_outcome_fee:      '0.0',
+        taker_uid:              taker.uid,
+        taker_income_unit:      'btc',
+        taker_income_amount:    '14.0',
+        taker_income_fee:       '0.021',
+        taker_outcome_unit:     'usd',
+        taker_outcome_amount:   '0.42',
+        taker_outcome_fee:      '0.0',
+        completed_at:           completed_at.iso8601
+      }).once
+    end
+
+    after do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    it 'publishes event', clean_database_with_truncation: true do
+      subject
+      expect(order_bid_taker.reload.state).to eq 'done'
+      expect(order_ask_maker.reload.state).to eq 'wait'
+    end
   end
 
-  subject { executor.execute! }
+  context 'maker buy order' do
+    let :executor do
+      bid = Matching::LimitOrder.new(order_bid_maker.to_matching_attributes)
+      ask = Matching::LimitOrder.new(order_ask_taker.to_matching_attributes)
+      Matching::Executor.new \
+        action: 'execute',
+        trade: {
+          market_id:    :btcusd,
+          maker_order_id:       bid.id,
+          taker_order_id:       ask.id,
+          strike_price: '0.03',
+          amount:       '14.0',
+          total:        '0.42'
+        }
+    end
 
-  before do
-    maker.ac(:btc).plus_funds('100.0'.to_d)
-    maker.ac(:btc).lock_funds('100.0'.to_d)
-  end
+    subject { executor.execute! }
 
-  before do
-    taker.ac(:usd).plus_funds('100.0'.to_d)
-    taker.ac(:usd).lock_funds('14.0'.to_d)
-  end
+    before do
+      taker.ac(:btc).plus_funds('100.0'.to_d)
+      taker.ac(:btc).lock_funds('100.0'.to_d)
+    end
 
-  before { Trade.any_instance.expects(:created_at).returns(completed_at).at_least_once }
+    before do
+      maker.ac(:usd).plus_funds('100.0'.to_d)
+      maker.ac(:usd).lock_funds('14.0'.to_d)
+    end
 
-  before do
-    DatabaseCleaner.clean
-    EventAPI.expects(:notify).with('market.btcusd.order_created', anything).twice
-    EventAPI.expects(:notify).with('market.btcusd.order_updated', anything).once
-    EventAPI.expects(:notify).with('market.btcusd.order_completed', anything).once
-    EventAPI.expects(:notify).with('market.btcusd.trade_completed', {
-      id:                     1,
-      market:                 'btcusd',
-      price:                  '0.03',
-      maker_uid:              maker.uid,
-      maker_income_unit:      'btc',
-      maker_income_amount:    '14.0',
-      maker_income_fee:       '0.021',
-      maker_outcome_unit:     'usd',
-      maker_outcome_amount:   '0.42',
-      maker_outcome_fee:      '0.0',
-      taker_uid:              taker.uid,
-      taker_income_unit:      'usd',
-      taker_income_amount:    '0.42',
-      taker_income_fee:       '0.00063',
-      taker_outcome_unit:     'btc',
-      taker_outcome_amount:   '14.0',
-      taker_outcome_fee:      '0.0',
-      completed_at:           completed_at.iso8601
-    }).once
-  end
+    before { Trade.any_instance.expects(:created_at).returns(completed_at).at_least_once }
 
-  after do
-    DatabaseCleaner.strategy = :truncation
-  end
+    before do
+      DatabaseCleaner.clean
+      EventAPI.expects(:notify).with('market.btcusd.order_created', anything).twice
+      EventAPI.expects(:notify).with('market.btcusd.order_updated', anything).once
+      EventAPI.expects(:notify).with('market.btcusd.order_completed', anything).once
+      EventAPI.expects(:notify).with('market.btcusd.trade_completed', {
+        id:                     1,
+        market:                 'btcusd',
+        price:                  '0.03',
+        amount:                 '14.0',
+        maker_uid:              maker.uid,
+        maker_income_unit:      'btc',
+        maker_income_amount:    '14.0',
+        maker_income_fee:       '0.021',
+        maker_outcome_unit:     'usd',
+        maker_outcome_amount:   '0.42',
+        maker_outcome_fee:      '0.0',
+        taker_uid:              taker.uid,
+        taker_income_unit:      'usd',
+        taker_income_amount:    '0.42',
+        taker_income_fee:       '0.00063',
+        taker_outcome_unit:     'btc',
+        taker_outcome_amount:   '14.0',
+        taker_outcome_fee:      '0.0',
+        completed_at:           completed_at.iso8601
+      }).once
+    end
 
-  it 'publishes event', clean_database_with_truncation: true do
-    subject
-    expect(order_bid.reload.state).to eq 'done'
-    expect(order_ask.reload.state).to eq 'wait'
+    after do
+      DatabaseCleaner.strategy = :truncation
+    end
+
+    it 'publishes event', clean_database_with_truncation: true do
+      subject
+      expect(order_bid_maker.reload.state).to eq 'done'
+      expect(order_ask_taker.reload.state).to eq 'wait'
+    end
   end
 end
