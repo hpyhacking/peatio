65f88d9e795c0d70a3b166a12e99ee74ec3477cb
Andrew Peresada <kohelbekker@gmail.com>
2020-05-27 17:34:09 +0300

    Add ability to set user rate limit (#2579)
    Co-authored-by: Maksym <mnaichuk@heliostech.fr>

--- app/api/v2/constraints.rb
+++ app/api/v2/constraints.rb
@@ -6,12 +6,13 @@ module API
     module Constraints
       class << self
         def included(base)
-          apply_rules!
+          rate_limit = ENV.fetch("PEATIO_RATE_LIMIT_5MIN", 6000).to_i
+          apply_rules!(rate_limit)
           base.use Rack::Attack
         end
 
-        def apply_rules!
-          Rack::Attack.throttle 'Limit number of calls to API', limit: 6000, period: 5.minutes do |req|
+        def apply_rules!(rate_limit)
+          Rack::Attack.throttle 'Limit number of calls to API', limit: rate_limit, period: 5.minutes do |req|
             req.env['api_v2.authentic_member_email']
           end
         end
