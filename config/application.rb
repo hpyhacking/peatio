ee222e53fb5522adb04af2f5d49fcf84f39d1656
Maksym <mnaichuk@heliostech.fr>
2020-12-25 14:43:39 +0200

    Enhancement: Cleanup application.yml (#2789)

--- config/application.rb
+++ config/application.rb
@@ -37,7 +37,7 @@ module Peatio
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    config.time_zone = ENV.fetch('TIMEZONE')
+    config.time_zone = ENV.fetch('TIMEZONE', 'UTC')
 
     # Configure relative url root by setting URL_ROOT_PATH environment variable.
     # Used by microkube with API Gateway.
