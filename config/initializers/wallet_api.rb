RyK-eR/peatio-1

Add opendax-cloud plugin. Rename opendax plugin.

config/initializers/wallet_api.rb
@@ -2,4 +2,6 @@ Peatio::Wallet.registry[:bitcoind] = Bitcoin::Wallet
 Peatio::Wallet.registry[:geth] = Ethereum::Wallet
 Peatio::Wallet.registry[:parity] = Ethereum::Wallet
 Peatio::Wallet.registry[:gnosis] = Gnosis::Wallet
-Peatio::Wallet.registry[:opendax] = Opendax::Wallet
+Peatio::Wallet.registry[:ow_hdwallet] = OWHDwallet::Wallet
+Peatio::Wallet.registry[:opendax] = OWHDwallet::Wallet
+Peatio::Wallet.registry[:openfinex_cloud] = OpendaxCloud::Wallet

hpyhacking/peatio

config/initializers/wallet_api.rb
@@ -2,4 +2,6 @@ Peatio::Wallet.registry[:bitcoind] = Bitcoin::Wallet
 Peatio::Wallet.registry[:geth] = Ethereum::Wallet
 Peatio::Wallet.registry[:parity] = Ethereum::Wallet
 Peatio::Wallet.registry[:gnosis] = Gnosis::Wallet
-Peatio::Wallet.registry[:opendax] = Opendax::Wallet
+Peatio::Wallet.registry[:ow_hdwallet] = OWHDWallet::Wallet
+Peatio::Wallet.registry[:opendax] = OWHDWallet::Wallet
+Peatio::Wallet.registry[:opendax_cloud] = OpendaxCloud::Wallet