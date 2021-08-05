687861ee08b07dcdca0810a7648f104355401c9f
Maksym Naichuk <mnaichuk@openware.com>
2021-08-05 21:46:16 +0300

    Fix: Change AddAccountType migration date

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_27_101029) do
+ActiveRecord::Schema.define(version: 2021_08_05_134633) do
 
   create_table "accounts", primary_key: ["currency_id", "member_id"], options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "member_id", null: false
@@ -251,7 +251,7 @@ ActiveRecord::Schema.define(version: 2021_07_27_101029) do
 
   create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "uid", limit: 32, null: false
-    t.string "email", null: false
+    t.string "email"
     t.integer "level", null: false
     t.string "role", limit: 16, null: false
     t.string "group", limit: 32, default: "vip-0", null: false
