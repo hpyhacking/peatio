06226e25fe79492283f72b13d382c5a55afe196d
Maksym Naichuk <mnaichuk@openware.com>
2021-09-26 20:51:53 +0300

    Fix account type migration for postgresql

--- db/migrate/20210805134633_add_account_type.rb
+++ db/migrate/20210805134633_add_account_type.rb
@@ -7,7 +7,14 @@ class AddAccountType < ActiveRecord::Migration[5.2]
       remove_index :operations_accounts, column: %i[type kind currency_type]
       add_index :operations_accounts, %i[type kind currency_type code], name: 'index_operations_accounts_on_type_kind_currency_type_code'
       if ActiveRecord::Base.connection.primary_key('accounts') == %w[currency_id member_id]
-        execute('ALTER TABLE accounts DROP PRIMARY KEY, ADD PRIMARY KEY(currency_id, member_id, type)')
+        adapter_type = connection.adapter_name.downcase.to_sym
+        case adapter_type
+        when :mysql, :mysql2
+          execute('ALTER TABLE accounts DROP PRIMARY KEY, ADD PRIMARY KEY(currency_id, member_id, type)')
+        when :postgresql
+          execute('ALTER TABLE accounts DROP CONSTRAINT accounts_pkey')
+          execute('ALTER TABLE accounts ADD PRIMARY KEY(currency_id, member_id, type)')
+        end
       end
     end
   end
--- db/migrate/20210924093524_change_account_primary_key.rb
+++ db/migrate/20210924093524_change_account_primary_key.rb
@@ -1,13 +1,27 @@
 class ChangeAccountPrimaryKey < ActiveRecord::Migration[5.2]
   def up
     if ActiveRecord::Base.connection.primary_key('accounts') == %w[currency_id member_id]
-      execute('ALTER TABLE accounts DROP PRIMARY KEY, ADD PRIMARY KEY(currency_id, member_id, type)')
+      adapter_type = connection.adapter_name.downcase.to_sym
+      case adapter_type
+      when :mysql, :mysql2
+        execute('ALTER TABLE accounts DROP PRIMARY KEY, ADD PRIMARY KEY(currency_id, member_id, type)')
+      when :postgresql
+        execute('ALTER TABLE accounts DROP CONSTRAINT accounts_pkey')
+        execute('ALTER TABLE accounts ADD PRIMARY KEY(currency_id, member_id, type)')
+      end
     end
   end
 
   def down
     if ActiveRecord::Base.connection.primary_key('accounts') == %w[currency_id member_id type]
-      execute('ALTER TABLE accounts DROP PRIMARY KEY, ADD PRIMARY KEY(currency_id, member_id)')
+      adapter_type = connection.adapter_name.downcase.to_sym
+      case adapter_type
+      when :mysql, :mysql2
+        execute('ALTER TABLE accounts DROP PRIMARY KEY, ADD PRIMARY KEY(currency_id, member_id)')
+      when :postgresql
+        execute('ALTER TABLE accounts DROP CONSTRAINT accounts_pkey')
+        execute('ALTER TABLE accounts ADD PRIMARY KEY(currency_id, member_id)')
+      end
     end
   end
 end
