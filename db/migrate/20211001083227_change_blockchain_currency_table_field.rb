f9e8b9b542507711645d97ddef976e7fc37d1dd2
Maksym Naichuk <mnaichuk@openware.com>
2021-10-05 09:47:10 +0300

    Fix: update change_blockchain_currency_table_field migration

--- db/migrate/20211001083227_change_blockchain_currency_table_field.rb
+++ db/migrate/20211001083227_change_blockchain_currency_table_field.rb
@@ -1,11 +1,5 @@
 class ChangeBlockchainCurrencyTableField < ActiveRecord::Migration[5.2]
   def up
-    change_column :blockchain_currencies, :blockchain_key, :string, null: false
-    change_column :deposits, :blockchain_key, :string, null: false
-    change_column :withdraws, :blockchain_key, :string, null: false
-    change_column :payment_addresses, :blockchain_key, :string, null: false
-    change_column :beneficiaries, :blockchain_key, :string, null: false
-
     adapter_type = connection.adapter_name.downcase.to_sym
     case adapter_type
     when :mysql, :mysql2
@@ -19,24 +13,30 @@ class ChangeBlockchainCurrencyTableField < ActiveRecord::Migration[5.2]
     fiat_blockchain = Blockchain.find_by(client: 'fiat')
 
     PaymentAddress.all.where(blockchain_key: nil).find_each(batch_size: 100) do |payment_address|
-      payment_address.update_columns(blockchain_key: fiat_blockchain.blockchain_key)
+      payment_address.update_columns(blockchain_key: fiat_blockchain.key)
     end
 
     Beneficiary.where(blockchain_key: nil).find_each(batch_size: 100) do |beneficiary|
-      beneficiary.update_columns(blockchain_key: fiat_blockchain.blockchain_key)
+      beneficiary.update_columns(blockchain_key: fiat_blockchain.key)
     end
 
     Withdraw.where(blockchain_key: nil).find_each(batch_size: 100) do |withdraws|
-      withdraws.update_columns(blockchain_key: fiat_blockchain.blockchain_key)
+      withdraws.update_columns(blockchain_key: fiat_blockchain.key)
     end
 
     Deposit.where(blockchain_key: nil).find_each(batch_size: 100) do |deposit|
-      deposit.update_columns(blockchain_key: fiat_blockchain.blockchain_key)
+      deposit.update_columns(blockchain_key: fiat_blockchain.key)
     end
 
     BlockchainCurrency.where(blockchain_key: nil).find_each(batch_size: 100) do |blockchain_currency|
-      blockchain_currency.update_columns(blockchain_key: fiat_blockchain.blockchain_key)
+      blockchain_currency.update_columns(blockchain_key: fiat_blockchain.key)
     end
+
+    change_column :blockchain_currencies, :blockchain_key, :string, null: false
+    change_column :deposits, :blockchain_key, :string, null: false
+    change_column :withdraws, :blockchain_key, :string, null: false
+    change_column :payment_addresses, :blockchain_key, :string, null: false
+    change_column :beneficiaries, :blockchain_key, :string, null: false
   end
 
   def down
